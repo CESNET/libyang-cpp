ecf46264542a59ae70845f83a7a4b6118ea178ff
Václav Kubernát <kubernat@cesnet.cz>
2021-06-10 01:19:11 +0200

    Add the DataNodeTerm class
    This class wraps the lyd_node_term struct. It represents a leaf or a
    leaf-list. Currently only supports a few operations.
    
    The libyang1-cpp equivalent was called Data_Node_Leaf_List and you would
    have to construct it by yourself. I don't want the user to construct any
    Node objects, so DataNodeTerm is created by the DataNode::asTerm()
    method. This works similar to the C-api - creating a DataNodeTerm class
    is essentially just a typecast with an additional check.
    
    Change-Id: I30cf5225209656d2673e2f17ca862124f17aeb37

--- include/libyang-cpp/DataNode.hpp
+++ include/libyang-cpp/DataNode.hpp
@@ -19,6 +19,7 @@ class Context;
 // Internal
 struct Empty;
 
+class DataNodeTerm;
 /**
  * @brief Class representing a node in a libyang tree.
  */
@@ -29,13 +30,30 @@ public:
     String printStr(const DataFormat format, const PrintFlags flags) const;
     std::optional<DataNode> findPath(const char* path) const;
     String path() const;
+    DataNodeTerm asTerm();
 
     friend Context;
+
+protected:
+    lyd_node* m_node;
 private:
     DataNode(lyd_node* node);
     DataNode(lyd_node* node, std::shared_ptr<Empty> viewCount);
 
-    lyd_node* m_node;
     std::shared_ptr<Empty> m_viewCount;
+};
+
+/**
+ * @brief Class representing a term node - leaf or leaf-list.
+ */
+class DataNodeTerm : DataNode {
+public:
+    using DataNode::path;
+
+    std::string_view valueStr() const;
+
+    friend DataNode;
+private:
+    using DataNode::DataNode;
 };
 }
