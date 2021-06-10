8ed62bcc01708f94e034f1fa705b0f1d0b85b418
Jan Kundrát <jan.kundrat@cesnet.cz>
2021-06-10 07:58:28 +0200

    CI: build the docs and converage info as usual
    Change-Id: I62652f89a3cb543bbb130aee666aa256f780008d

--- ci/build.sh
+++ ci/build.sh
@@ -7,6 +7,7 @@ ZUUL_JOB_NAME=$(jq < ~/zuul-env.json -r '.job')
 ZUUL_TENANT=$(jq < ~/zuul-env.json -r '.tenant')
 ZUUL_PROJECT_SRC_DIR=$HOME/$(jq < ~/zuul-env.json -r '.project.src_dir')
 ZUUL_PROJECT_SHORT_NAME=$(jq < ~/zuul-env.json -r '.project.short_name')
+ZUUL_PROJECT_NAME=$(jq < ~/zuul-env.json -r '.project.name')
 ZUUL_GERRIT_HOSTNAME=$(jq < ~/zuul-env.json -r '.project.canonical_hostname')
 
 CI_PARALLEL_JOBS=$(awk -vcpu=$(getconf _NPROCESSORS_ONLN) 'BEGIN{printf "%.0f", cpu*1.3+1}')
@@ -69,4 +70,16 @@ build_n_test() {
 
 build_n_test github/CESNET/libyang -DENABLE_BUILD_TESTS=ON -DENABLE_VALGRIND_TESTS=OFF
 build_n_test github/onqtam/doctest -DDOCTEST_WITH_TESTS=OFF
-build_n_test CzechLight/libyang-cpp -DBUILD_TESTING=ON
+build_n_test ${ZUUL_PROJECT_NAME} -DBUILD_TESTING=ON
+
+pushd ${BUILD_DIR}/${ZUUL_PROJECT_NAME}
+if [[ $JOB_PERFORM_EXTRA_WORK == 1 ]]; then
+    ninja-build doc
+    pushd html
+    zip -r ~/zuul-output/docs/html.zip .
+    popd
+fi
+
+if [[ $LDFLAGS =~ .*--coverage.* ]]; then
+    gcovr -j ${CI_PARALLEL_JOBS} --object-directory ${BUILD_DIR}/${ZUUL_PROJECT_NAME} --root ${ZUUL_PROJECT_SRC_DIR} --xml --output ${BUILD_DIR}/${ZUUL_PROJECT_NAME}/coverage.xml
+fi
