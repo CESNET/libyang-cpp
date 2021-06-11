ab8e0b4c4f7511514ccfbcd81b852f5408dbcc22
Václav Kubernát <kubernat@cesnet.cz>
2021-06-11 06:23:23 +0200

    Implement value retrieval
    Decimal64 implementation is imported from cla-sysrepo, commit
    6a2b76cabe3d433c83a20bddf59449cdd8b130d2 with one change - I made the
    ctor public.
    
    Change-Id: I4c14fa0161cad651fcc7c299a3c0e56db510741d

--- include/libyang-cpp/DataNode.hpp
+++ include/libyang-cpp/DataNode.hpp
@@ -11,6 +11,7 @@
 #include <optional>
 #include <libyang-cpp/Enum.hpp>
 #include <libyang-cpp/String.hpp>
+#include <libyang-cpp/Value.hpp>
 
 struct lyd_node;
 struct ly_ctx;
@@ -39,6 +40,9 @@ public:
     std::optional<DataNode> newPath(const char* path, const char* value = nullptr, const std::optional<CreationOptions> options = std::nullopt);
 
     friend Context;
+    friend DataNodeTerm;
+
+    bool operator==(const DataNode& node) const;
 
     friend std::optional<DataNode> impl::newPath(lyd_node* node, ly_ctx* parent, std::shared_ptr<internal_empty> viewCount, const char* path, const char* value, const std::optional<CreationOptions> options);
 
@@ -62,6 +66,8 @@ public:
     std::string_view valueStr() const;
 
     friend DataNode;
+    Value value() const;
+
 private:
     using DataNode::DataNode;
 };
