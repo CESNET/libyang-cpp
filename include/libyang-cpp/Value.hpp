1684205b04eff126bbf87d69785e0d8fc9ffc225
Václav Kubernát <kubernat@cesnet.cz>
2021-06-11 06:23:23 +0200

    Implement value retrieval
    Change-Id: I4c14fa0161cad651fcc7c299a3c0e56db510741d

---
+++ include/libyang-cpp/Value.hpp
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2021 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Václav Kubernát <kubernat@cesnet.cz>
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+*/
+#pragma once
+#include <cstdint>
+#include <string>
+#include <variant>
+#include <vector>
+
+namespace libyang {
+/**
+ * @brief Represents a YANG value of type `empty`.
+ */
+struct Empty {
+    auto operator<=>(const Empty&) const = default;
+};
+
+/**
+ * Represents a YANG value of type `binary` as raw bytes and as a base64 string.
+ */
+struct Binary {
+    auto operator<=>(const Binary&) const = default;
+    std::vector<uint8_t> data;
+    std::string base64;
+};
+
+class DataNodeTerm;
+using Value = std::variant<
+    int8_t,
+    int16_t,
+    int32_t,
+    int64_t,
+    uint8_t,
+    uint16_t,
+    uint32_t,
+    uint64_t,
+    bool,
+    Empty,
+    Binary,
+    std::string
+>;
+}
