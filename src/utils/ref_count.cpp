22fc4a91eef7c6ee083f9f5d8431626be2b1b4ff
Václav Kubernát <kubernat@cesnet.cz>
2021-06-17 20:41:11 +0200

    Keep reference to context in data nodes
    If Context goes out of scope before a DataNode does, it creates a
    dangling reference, because libyang doesn't have any kind of special
    handling for freeing ly_ctx before lyd_node.
    
    Now, all DataNode instances contain a shared_ptr to the ly_ctx.
    
    Change-Id: I0720b5eaabc599927acca35dc9fb037783505066

---
+++ src/utils/ref_count.cpp
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2021 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Václav Kubernát <kubernat@cesnet.cz>
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+*/
+#include "ref_count.hpp"
+
+namespace libyang {
+internal_refcount::internal_refcount(std::shared_ptr<ly_ctx> ctx, std::set<DataNode*> nodes)
+    : nodes(nodes)
+    , context(ctx)
+{
+}
+}
--- src/utils/ref_count.hpp
+++ src/utils/ref_count.hpp
@@ -6,11 +6,15 @@
  * SPDX-License-Identifier: BSD-3-Clause
 */
 #pragma once
+#include <memory>
 #include <set>
 
+struct ly_ctx;
 namespace libyang {
 class DataNode;
 struct internal_refcount {
+    explicit internal_refcount(std::shared_ptr<ly_ctx> ctx, std::set<DataNode*> nodes = {});
     std::set<DataNode*> nodes;
+    std::shared_ptr<ly_ctx> context;
 };
 }
