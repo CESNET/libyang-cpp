a139910f20c37f7bae09e620cf2c3ed887c9d333
Václav Kubernát <kubernat@cesnet.cz>
2021-06-30 08:00:03 +0200

    Add type info for identityref
    Change-Id: I08a9ebd05a07d79e073fd53cdfa76849b7e0b9ed

--- include/libyang-cpp/Type.hpp
+++ include/libyang-cpp/Type.hpp
@@ -13,12 +13,14 @@
 #include <vector>
 
 struct ly_ctx;
+struct lysc_ident;
 struct lysc_type;
 
 namespace libyang {
 class Leaf;
 namespace types {
     class Enumeration;
+    class IdentityRef;
 }
 /**
  * @brief Contains information about leaf's type.
@@ -28,6 +30,7 @@ public:
     LeafBaseType base() const;
 
     types::Enumeration asEnum() const;
+    types::IdentityRef asIdentityRef() const;
     friend Leaf;
 
 protected:
@@ -37,6 +40,19 @@ protected:
 private:
     Type(const lysc_type* type, std::shared_ptr<ly_ctx> ctx);
 
+};
+
+class Identity {
+public:
+    friend types::IdentityRef;
+    std::vector<Identity> derived() const;
+    std::string_view name() const;
+
+private:
+    Identity(const lysc_ident* ident, std::shared_ptr<ly_ctx> ctx);
+
+    const lysc_ident* m_ident;
+    std::shared_ptr<ly_ctx> m_ctx;
 };
 
 namespace types {
@@ -54,6 +70,16 @@ public:
     };
 
     std::vector<Enum> items() const;
+
+private:
+    using Type::Type;
+};
+
+class IdentityRef : public Type {
+public:
+    friend Type;
+
+    std::vector<Identity> bases() const;
 
 private:
     using Type::Type;
