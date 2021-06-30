38348689d4f84e47b5d03b91b8601ed72eb103ba
Václav Kubernát <kubernat@cesnet.cz>
2021-06-30 08:07:04 +0200

    Refactor pretty printers
    Change-Id: I77ba22d50526bb31e0aa386bab48d3eb2e3bd07f

--- tests/data_node.cpp
+++ tests/data_node.cpp
@@ -7,104 +7,10 @@
 */
 
 #include <doctest/doctest.h>
-#include <experimental/iterator>
 #include <libyang-cpp/Context.hpp>
 #include <libyang-cpp/utils/exception.hpp>
-#include <sstream>
 #include "example_schema.hpp"
-
-using namespace std::string_literals;
-
-namespace libyang {
-    namespace {
-        struct impl_toStruct {
-            std::string operator()(const Binary& value)
-            {
-                std::ostringstream oss;
-                oss << std::hex;
-                std::transform(value.data.begin(), value.data.end(), std::experimental::make_ostream_joiner(oss, ", "), [] (uint8_t num) {
-                    return std::to_string(static_cast<unsigned int>(num));
-                });
-                return oss.str();
-            }
-
-            std::string operator()(const Empty)
-            {
-                return "<empty>";
-            }
-
-            std::string operator()(const std::string& value)
-            {
-                return value;
-            }
-
-            std::string operator()(const std::optional<DataNode>& value)
-            {
-                if (!value) {
-                    return "std::nullopt (DataNode)";
-                }
-
-                return std::string{value->path()};
-            }
-
-            std::string operator()(const Decimal64& value)
-            {
-                return "Decimal64{"s + std::to_string(value.number) + ", " + std::to_string(value.digits) + "}";
-            }
-
-            std::string operator()(const std::vector<Bit>& value)
-            {
-                std::ostringstream oss;
-                oss << "Bits{";
-                std::transform(value.begin(), value.end(), std::experimental::make_ostream_joiner(oss, ", "), [] (const Bit& bit) {
-                    return bit.name + "(" + std::to_string(bit.position) + ")";
-                });
-                oss << "}";
-                return oss.str();
-            }
-
-            std::string operator()(const Enum& value)
-            {
-                return "Enum{"s + value.name + "}";
-            }
-
-            std::string operator()(const IdentityRef& value)
-            {
-                return "IdentityRef{"s + value.module + ":" + value.name + "}";
-            }
-
-            template <typename Type>
-            std::string operator()(const Type& value)
-            {
-                return std::to_string(value);
-            }
-        };
-    }
-    doctest::String toString(const Value& value) {
-        auto str = std::visit(impl_toStruct{}, value);
-        return str.c_str();
-    }
-    doctest::String toString(const std::vector<libyang::DataNode>& nodes) {
-        std::ostringstream oss;
-        std::transform(nodes.begin(), nodes.end(), std::experimental::make_ostream_joiner(oss, ", "), [] (const DataNode& node) {
-            return "DataNode -> " + std::string{node.path()};
-        });
-
-        return oss.str().c_str();
-    }
-}
-
-namespace std {
-doctest::String toString(const std::vector<std::string>& vec) {
-    std::ostringstream oss;
-    oss << "std::vector<std::string>{\n    ";
-    std::copy(vec.begin(), vec.end(), std::experimental::make_ostream_joiner(oss, ",\n    "));
-
-    oss << "\n}";
-
-    return oss.str().c_str();
-}
-}
+#include "pretty_printers.hpp"
 
 const auto data = R"({
   "example-schema:leafInt32": 420
@@ -153,12 +59,6 @@ const auto data2 = R"({
   }
 }
 )";
-
-namespace libyang {
-doctest::String toString(const String& value) {
-    return std::string{value}.c_str();
-}
-}
 
 TEST_CASE("Data Node manipulation")
 {
