7f9847cc5bbcfe266f0b335525aa74e18bdfca44
Václav Kubernát <kubernat@cesnet.cz>
2021-07-08 08:57:43 +0200

    Implement ChildInstantiables iteration
    Change-Id: Icc39cd17ee6ba8bdc0a51bf5938b483ebfc3d5d1

--- src/SchemaNode.cpp
+++ src/SchemaNode.cpp
@@ -8,6 +8,7 @@
 
 #include <libyang/tree.h>
 #include <libyang/tree_schema.h>
+#include <libyang-cpp/ChildInstantiables.hpp>
 #include <libyang-cpp/SchemaNode.hpp>
 #include <libyang-cpp/utils/exception.hpp>
 #include <libyang/libyang.h>
@@ -41,6 +42,14 @@ String SchemaNode::path() const
 std::string_view SchemaNode::name() const
 {
     return m_node->name;
+}
+
+/**
+ * Returns a collection of data instantiable children.
+ */
+ChildInstanstiables SchemaNode::childInstantiables() const
+{
+    return ChildInstanstiables{m_node, nullptr, m_ctx};
 }
 
 /**
