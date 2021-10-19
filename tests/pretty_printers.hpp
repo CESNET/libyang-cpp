446cdffa4735747fb1716da657592fec058489ba
Václav Kubernát <kubernat@cesnet.cz>
2021-10-19 17:26:40 +0200

    Implement insertBefore and insertAfter
    Change-Id: Ic05fd089d26b5936dc56de2b66e26b3b08b2f52a

--- tests/pretty_printers.hpp
+++ tests/pretty_printers.hpp
@@ -25,6 +25,17 @@ doctest::String toString(const std::vector<std::string>& vec)
 
     return oss.str().c_str();
 }
+
+doctest::String toString(const std::vector<int32_t>& vec)
+{
+    std::ostringstream oss;
+    oss << "std::vector<std::string>{\n    ";
+    std::copy(vec.begin(), vec.end(), std::experimental::make_ostream_joiner(oss, ",\n    "));
+
+    oss << "\n}";
+
+    return oss.str().c_str();
+}
 }
 
 namespace libyang {
