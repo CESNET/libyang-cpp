38b5fb5532485f34e6d3c1e18e5993343ac65405
Václav Kubernát <kubernat@cesnet.cz>
2021-10-21 15:28:18 +0200

    Add DataNode::merge
    Change-Id: I304f9cc567c0f31930655a0e5fca9a2b1c11c6cc

--- src/DataNode.cpp
+++ src/DataNode.cpp
@@ -513,6 +513,19 @@ void DataNode::insertBefore(DataNode toInsert)
     }, m_refs);
 }
 
+/**
+ * Merges `toMerge` into `this`. After the operation, `this` will always point to the first sibling.
+ * Both `this` and `toMerge` must be a top-level node.
+ */
+void DataNode::merge(DataNode toMerge)
+{
+    toMerge.unlink();
+
+    handleLyTreeOperation({&toMerge}, [this, &toMerge] {
+        lyd_merge_tree(&this->m_node, toMerge.m_node, 0);
+    }, m_refs);
+}
+
 std::string_view DataNodeTerm::valueStr() const
 {
     return lyd_get_value(m_node);
