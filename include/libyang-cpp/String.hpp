8324c145ffc113b61d6a7e180b08c7e6719f2d17
Václav Kubernát <kubernat@cesnet.cz>
2021-10-26 05:06:47 +0200

    Add String::operator==(String)
    Makes the class a little more usable.
    
    Change-Id: I7edef1f31c46d3931e9092604772d2da4228f742

--- include/libyang-cpp/String.hpp
+++ include/libyang-cpp/String.hpp
@@ -22,6 +22,7 @@ public:
 
     bool operator==(const char*) const;
     bool operator==(const std::string_view& str) const;
+    bool operator==(const String& str) const;
 
     friend std::ostream& operator<<(std::ostream& os, const String& str);
 
