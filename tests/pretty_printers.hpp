fe94d68827c988e2e8ece695c82ce4197027c77e
Václav Kubernát <kubernat@cesnet.cz>
2022-01-28 06:15:29 +0100

    pretty_printers: Use ValuePrinter
    Now that we have ValuePrinter, we don't the custom toString visitor.
    
    Change-Id: Iac8c00f3d85f1b3bd52f8b3fee379b5d9403be24

--- tests/pretty_printers.hpp
+++ tests/pretty_printers.hpp
@@ -76,73 +76,9 @@ doctest::String toString(const String& value)
     return std::string{value}.c_str();
 }
 
-namespace {
-struct impl_toStruct {
-    std::string operator()(const Binary& value)
-    {
-        std::ostringstream oss;
-        oss << std::hex;
-        std::transform(value.data.begin(), value.data.end(), std::experimental::make_ostream_joiner(oss, ", "), [](uint8_t num) {
-            return std::to_string(static_cast<unsigned int>(num));
-        });
-        return oss.str();
-    }
-
-    std::string operator()(const Empty)
-    {
-        return "<empty>";
-    }
-
-    std::string operator()(const std::string& value)
-    {
-        return value;
-    }
-
-    std::string operator()(const std::optional<DataNode>& value)
-    {
-        if (!value) {
-            return "std::nullopt (DataNode)";
-        }
-
-        return std::string{value->path()};
-    }
-
-    std::string operator()(const Decimal64& value)
-    {
-        return "Decimal64{"s + std::to_string(value.number) + ", " + std::to_string(value.digits) + "}";
-    }
-
-    std::string operator()(const std::vector<Bit>& value)
-    {
-        std::ostringstream oss;
-        oss << "Bits{";
-        std::transform(value.begin(), value.end(), std::experimental::make_ostream_joiner(oss, ", "), [](const Bit& bit) {
-            return bit.name + "(" + std::to_string(bit.position) + ")";
-        });
-        oss << "}";
-        return oss.str();
-    }
-
-    std::string operator()(const Enum& value)
-    {
-        return "Enum{"s + value.name + "}";
-    }
-
-    std::string operator()(const IdentityRef& value)
-    {
-        return "IdentityRef{"s + value.module + ":" + value.name + "}";
-    }
-
-    template <typename Type>
-    std::string operator()(const Type& value)
-    {
-        return std::to_string(value);
-    }
-};
-}
 doctest::String toString(const Value& value)
 {
-    auto str = std::visit(impl_toStruct{}, value);
+    auto str = std::visit(ValuePrinter{}, value);
     return str.c_str();
 }
 doctest::String toString(const std::vector<libyang::DataNode>& nodes)
