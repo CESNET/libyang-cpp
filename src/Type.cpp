CESNET/libyang-cpp

commit 1681fe98b06f616138cc24e7598480822e18aee6

schema: make string's `length` statement available
String's `length` statement is implemented as types::String::Length which allows only one error-app-tag, description for whole statement due to YANG's grammar. Parts of `length` statement are available as
types::String::Length::LengthPart.

Change-Id: Ibe4aac77cf2e3be84c4779560c7702a87aec20ac

src/Type.cpp
@@ -334,4 +334,29 @@ std::vector<types::String::Pattern> types::String::patterns() const
     }
     return res;
 }
+
+/**
+ * @brief Returns the contents of the `length` statement of the a string-based leaf.
+ */
+types::Length types::String::length() const
+{
+    throwIfParsedUnavailable();
+
+    auto str = reinterpret_cast<const lysc_type_str*>(m_type);
+
+    std::vector<Length::Part> parts;
+    for (const auto& it : std::span(str->length->parts, LY_ARRAY_COUNT(str->length->parts))) {
+        parts.emplace_back(Length::Part{
+            .min = it.min_u64,
+            .max = it.max_u64,
+        });
+    }
+
+    return Length{
+        .parts = parts,
+        .description = str->length->dsc ? std::optional<std::string>{str->length->dsc} : std::nullopt,
+        .errorAppTag = str->length->eapptag ? std::optional<std::string>{str->length->eapptag} : std::nullopt,
+        .errorMessage = str->length->emsg ? std::optional<std::string>{str->length->emsg} : std::nullopt,
+    };
+}
 }

tests/schema_node.cpp
@@ -41,14 +41,25 @@ module type_module {
             type string;
         }
 
-        leaf notKey {
+        leaf notKey1 {
             type string {
+              length "10 .. 20 | 50 .. 100 | 255";
               pattern "fo+";
               pattern "XXX" {
                 description "yay";
                 error-app-tag "x-XXX-failed";
                 error-message "hard to fail this one";
                 modifier invert-match;
+              }
+            }
+        }
+
+        leaf notKey2 {
+            type string {
+                length "min .. max" {
+                    description "yay";
+                    error-app-tag "x-XXX-failed";
+                    error-message "hard to fail this one";
               }
             }
         }
@@ -494,7 +505,8 @@ TEST_CASE("SchemaNode")
         {
             expectedPaths = {
                 "/type_module:myList/lol",
-                "/type_module:myList/notKey"
+                "/type_module:myList/notKey1",
+                "/type_module:myList/notKey2",
             };
 
             children = ctx->findPath("/type_module:myList").childInstantiables();
@@ -683,9 +695,9 @@ TEST_CASE("SchemaNode")
 
     DOCTEST_SUBCASE("String::patterns")
     {
-        REQUIRE_THROWS_WITH_AS(ctx->findPath("/type_module:myList/notKey").asLeaf().valueType().asString().patterns(), "Context not created with libyang::ContextOptions::SetPrivParsed", libyang::Error);
+        REQUIRE_THROWS_WITH_AS(ctx->findPath("/type_module:myList/notKey1").asLeaf().valueType().asString().patterns(), "Context not created with libyang::ContextOptions::SetPrivParsed", libyang::Error);
         REQUIRE_THROWS_WITH_AS(ctxWithParsed->findPath("/example-schema:typedefedLeafInt").asLeaf().valueType().asString().patterns(), "Type is not a string", libyang::Error);
-        auto s_type = ctxWithParsed->findPath("/type_module:myList/notKey").asLeaf().valueType().asString();
+        auto s_type = ctxWithParsed->findPath("/type_module:myList/notKey1").asLeaf().valueType().asString();
         REQUIRE(s_type.patterns().size() == 2);
         REQUIRE(s_type.patterns()[0].pattern == "fo+");
         REQUIRE(!s_type.patterns()[0].isInverted);
@@ -697,5 +709,29 @@ TEST_CASE("SchemaNode")
         REQUIRE(s_type.patterns()[1].description == "yay");
         REQUIRE(s_type.patterns()[1].errorAppTag == "x-XXX-failed");
         REQUIRE(s_type.patterns()[1].errorMessage == "hard to fail this one");
+    }
+
+    DOCTEST_SUBCASE("String::length")
+    {
+        REQUIRE_THROWS_WITH_AS(ctx->findPath("/type_module:myList/notKey1").asLeaf().valueType().asString().length(), "Context not created with libyang::ContextOptions::SetPrivParsed", libyang::Error);
+        REQUIRE_THROWS_WITH_AS(ctxWithParsed->findPath("/example-schema:typedefedLeafInt").asLeaf().valueType().asString().length(), "Type is not a string", libyang::Error);
+        auto s_type1 = ctxWithParsed->findPath("/type_module:myList/notKey1").asLeaf().valueType().asString();
+        REQUIRE(s_type1.length().parts.size() == 3);
+        REQUIRE(s_type1.length().parts[0].min == 10);
+        REQUIRE(s_type1.length().parts[0].max == 20);
+        REQUIRE(s_type1.length().parts[1].min == 50);
+        REQUIRE(s_type1.length().parts[1].max == 100);
+        REQUIRE(s_type1.length().parts[2].min == 255);
+        REQUIRE(s_type1.length().parts[2].max == 255);
+        REQUIRE(!s_type1.length().description);
+        REQUIRE(!s_type1.length().errorAppTag);
+        REQUIRE(!s_type1.length().errorMessage);
+        auto s_type2 = ctxWithParsed->findPath("/type_module:myList/notKey2").asLeaf().valueType().asString();
+        REQUIRE(s_type2.length().parts.size() == 1);
+        REQUIRE(s_type2.length().parts[0].min == 0);
+        REQUIRE(s_type2.length().parts[0].max == std::numeric_limits<uint64_t>::max());
+        REQUIRE(s_type2.length().description == "yay");
+        REQUIRE(s_type2.length().errorAppTag == "x-XXX-failed");
+        REQUIRE(s_type2.length().errorMessage == "hard to fail this one");
     }
 }

include/libyang-cpp/Type.hpp
@@ -141,6 +141,28 @@ private:
 };
 
 /**
+ * @brief Contains information about the `length` statement, not to be used for `range` statement.
+ *
+ * Wraps `struct lysc_range`.
+ */
+struct LIBYANG_CPP_EXPORT Length {
+    /**
+     * @brief Contains information about pair of possible (min/max) length
+     *
+     * Wraps `struct lysc_range_part`.
+     */
+    struct LIBYANG_CPP_EXPORT Part {
+        uint64_t min;
+        uint64_t max;
+    };
+
+    std::vector<Part> parts;
+    std::optional<std::string> description;
+    std::optional<std::string> errorAppTag;
+    std::optional<std::string> errorMessage;
+};
+
+/**
  * @brief Contains information about the `leafref` leaf type.
  */
 class LIBYANG_CPP_EXPORT LeafRef : public Type {
@@ -217,6 +239,7 @@ public:
     };
 
     std::vector<Pattern> patterns() const;
+    Length length() const;
 
     friend Type;
 

commit bf0d2f2deabef26232c79db59814aeb09340fac5

fix crash on strings with no `length` statement
The entire (struct lysc_type_str*)::length can be a nullptr. This is
different from the `pattern` statement where the descriptions and error
tags/messages are defined on a per-chunk basis, and therefore the code
would never try to dereference a null pointer.

Fixes: 1681fe9 schema: make string's `length` statement available
Change-Id: I1302f209897cbba737346e93095a84a31b9fddfb

src/Type.cpp
@@ -317,17 +317,17 @@ std::vector<Type> types::Union::types() const
 namespace {
 template <typename T> std::optional<std::string> extractDescription(T& struct_ptr)
 {
-    return struct_ptr->dsc ? std::optional<std::string>{struct_ptr->dsc} : std::nullopt;
+    return (struct_ptr && struct_ptr->dsc) ? std::optional<std::string>{struct_ptr->dsc} : std::nullopt;
 }
 
 template <typename T> std::optional<std::string> extractErrAppTag(T& struct_ptr)
 {
-    return struct_ptr->eapptag ? std::optional<std::string>{struct_ptr->eapptag} : std::nullopt;
+    return (struct_ptr && struct_ptr->eapptag) ? std::optional<std::string>{struct_ptr->eapptag} : std::nullopt;
 }
 
 template <typename T> std::optional<std::string> extractErrMessage(T& struct_ptr)
 {
-    return struct_ptr->emsg ? std::optional<std::string>{struct_ptr->emsg} : std::nullopt;
+    return (struct_ptr && struct_ptr->emsg) ? std::optional<std::string>{struct_ptr->emsg} : std::nullopt;
 }
 }
 
@@ -362,11 +362,13 @@ types::Length types::String::length() const
     auto str = reinterpret_cast<const lysc_type_str*>(m_type);
 
     std::vector<Length::Part> parts;
-    for (const auto& it : std::span(str->length->parts, LY_ARRAY_COUNT(str->length->parts))) {
-        parts.emplace_back(Length::Part{
-            .min = it.min_u64,
-            .max = it.max_u64,
-        });
+    if (str->length) {
+        for (const auto& it : std::span(str->length->parts, LY_ARRAY_COUNT(str->length->parts))) {
+            parts.emplace_back(Length::Part{
+                .min = it.min_u64,
+                .max = it.max_u64,
+            });
+        }
     }
 
     return Length{

tests/schema_node.cpp
@@ -787,6 +787,9 @@ TEST_CASE("SchemaNode")
         REQUIRE(s_type2.length().description == "yay");
         REQUIRE(s_type2.length().errorAppTag == "x-XXX-failed");
         REQUIRE(s_type2.length().errorMessage == "hard to fail this one");
+
+        auto noLength = ctxWithParsed->findPath("/type_module:listAdvancedWithOneKey/lol").asLeaf().valueType().asString();
+        REQUIRE(noLength.length().parts.size() == 0);
     }
 
     DOCTEST_SUBCASE("String::patterns")