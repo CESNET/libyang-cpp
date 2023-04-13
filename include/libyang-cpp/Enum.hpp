CESNET/libyang-cpp

commit 1d7c822c4f57947a459454649eef5f7563f87e65

Make this buildable on Ubuntu 20.04 with GCC 10
...which has doctest 2.3.6, not 2.4.6 that we require.

Looks like the visibility rules are more complex (perhaps the operators
are auto-picked in GCC 11?), and indeed, GCC 10 requires that extra
attribute for visibility tweaking, otherwise we get this:

 /usr/bin/ld: CMakeFiles/test_schema_node.dir/tests/schema_node.cpp.o: in function `doctest::String doctest::detail::stringifyBinaryExpr<libyang::NodeType, libyang::NodeType>(libyang::NodeType const&, char const*, libyang::NodeType const&)':
 schema_node.cpp:(.text._ZN7doctest6detail19stringifyBinaryExprIN7libyang8NodeTypeES3_EENS_6StringERKT_PKcRKT0_[_ZN7doctest6detail19stringifyBinaryExprIN7libyang8NodeTypeES3_EENS_6StringERKT_PKcRKT0_]+0x43): undefined reference to `libyang::operator<<(std::ostream&, libyang::NodeType const&)'
 /usr/bin/ld: schema_node.cpp:(.text._ZN7doctest6detail19stringifyBinaryExprIN7libyang8NodeTypeES3_EENS_6StringERKT_PKcRKT0_[_ZN7doctest6detail19stringifyBinaryExprIN7libyang8NodeTypeES3_EENS_6StringERKT_PKcRKT0_]+0x66): undefined reference to `libyang::operator<<(std::ostream&, libyang::NodeType const&)'

This has an easy fix -- just add the LIBYANG_CPP_EXPORT after that
function, right (because if it's put before that function, it tries to
apply that attribute to the return type, std::string, and emits a
warning)? Not so fast, then we have MSVC which chokes on this:

 libyang-cpp\include\libyang-cpp/Enum.hpp(294,66): error C2144: syntax error: 'int' should be preceded by ';' [build-libyang-cpp\yang-cpp.vcxproj]
 libyang-cpp\include\libyang-cpp/Enum.hpp(294,84): warning C4091: '__declspec(dllexport)': ignored on left of 'int' when no variable is declared [build-libyang-cpp\yang-cpp.vcxproj]

Luckily, I'm not afraid to use these macros.

Change-Id: I370d85652542f4c731c6b01b6d7d484d2edbb987

include/libyang-cpp/Enum.hpp
@@ -9,6 +9,7 @@
 
 #include <cstdint>
 #include <iosfwd>
+#include <libyang-cpp/export.h>
 #include <type_traits>
 namespace libyang {
 /**
@@ -290,6 +291,14 @@ constexpr ParseOptions operator|(const ParseOptions a, const ParseOptions b)
     return implEnumBitOr(a, b);
 }
 
-std::ostream& operator<<(std::ostream& os, const NodeType& type);
-std::ostream& operator<<(std::ostream& os, const ErrorCode& err);
+std::ostream& operator<<(std::ostream& os, const NodeType& type)
+#if defined(__GNUC__) && !defined(__llvm__) && __GNUC__ <= 10
+    LIBYANG_CPP_EXPORT
+#endif
+    ;
+std::ostream& operator<<(std::ostream& os, const ErrorCode& err)
+#if defined(__GNUC__) && !defined(__llvm__) && __GNUC__ <= 10
+    LIBYANG_CPP_EXPORT
+#endif
+    ;
 }

CMakeLists.txt
@@ -65,7 +65,7 @@ include(GenerateExportHeader)
 generate_export_header(yang-cpp BASE_NAME libyang_cpp EXPORT_FILE_NAME libyang-cpp/export.h)
 
 if(BUILD_TESTING)
-    find_package(doctest 2.4.6 REQUIRED)
+    find_package(doctest 2.3.6 REQUIRED)
 
     add_library(DoctestIntegration STATIC
         tests/doctest-integration.cpp

commit 510d5ac5fe5f4da4acd191b83c4ba7e17aea344d

Fix symbol export/visibility
Turns out that this macro is needed also on Mac OS X builds (both with
GCC 11.3 for x86_64 and with Apple clang version 14.0.0
(clang-1400.0.29.202) for arm64-apple-darwin22.1.0). If I'm reading my
original commit message correctly, this was only choking on MSVC, so
let's use that macro everywhere but on MSVC.

Fixes: 1d7c822 Make this buildable on Ubuntu 20.04 with GCC 10
Bug: https://github.com/CESNET/libyang-cpp/issues/7
Change-Id: Ib3502057d17ab8d4a138de02204d0b8e1e1c6442

include/libyang-cpp/Enum.hpp
@@ -292,12 +292,12 @@ constexpr ParseOptions operator|(const ParseOptions a, const ParseOptions b)
 }
 
 std::ostream& operator<<(std::ostream& os, const NodeType& type)
-#if defined(__GNUC__) && !defined(__llvm__) && __GNUC__ <= 10
+#if !defined(_MSC_VER)
     LIBYANG_CPP_EXPORT
 #endif
     ;
 std::ostream& operator<<(std::ostream& os, const ErrorCode& err)
-#if defined(__GNUC__) && !defined(__llvm__) && __GNUC__ <= 10
+#if !defined(_MSC_VER)
     LIBYANG_CPP_EXPORT
 #endif
     ;