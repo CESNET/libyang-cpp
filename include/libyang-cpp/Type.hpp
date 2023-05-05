1a140cc0f18caf19fa353b0a531c31221f4a1d8a
Jan Kundrát <jan.kundrat@cesnet.cz>
2023-05-05 19:50:36 +0200

    Fix build failure with MSVC
    The recent patches which added support for reporting numeric
    ranges/limits of values were hit by a risk of a circular #include loop;
    the std::variant that we use for reporting literal values (`Value`) was
    needed from inside the `Type.hpp`. After quite a few tries, I settled on
    moving just the offending type (`libyang::types::Numeric`) into an extra
    include file.
    
    Change-Id: I5af3ed43ed9097094b7acd63672211afe0574967
    Fixes: 7565fe3 ("refactor: forward-declare the Value variant")
    Fixes: 5861137 ("schema: accessing numeric data limits")

--- include/libyang-cpp/Type.hpp
+++ include/libyang-cpp/Type.hpp
@@ -8,7 +8,6 @@
 #pragma once
 
 #include <libyang-cpp/Enum.hpp>
-#include <libyang-cpp/ValueFwd.hpp>
 #include <libyang-cpp/export.h>
 #include <memory>
 #include <optional>
@@ -250,34 +249,6 @@ public:
 
     friend Type;
 
-private:
-    using Type::Type;
-};
-
-/**
- * @brief Info about numeric data types -- {u,}int{8,16,32,64} and decimal64
- *
- * Wraps `lysc_type_number` or `lysc_type_dec`.
- */
-class LIBYANG_CPP_EXPORT Numeric : public Type {
-public:
-    /**
-     * @brief Contains information about the `range` statement
-     *
-     * Wraps `struct lysc_range` for numeric data types.
-     * */
-    struct LIBYANG_CPP_EXPORT Range {
-        using Part = std::pair<Value, Value>;
-        std::vector<Part> parts;
-        std::optional<std::string> description;
-        std::optional<std::string> errorAppTag;
-        std::optional<std::string> errorMessage;
-    };
-
-    Range range() const;
-    uint8_t fractionDigits() const;
-
-    friend Type;
 private:
     using Type::Type;
 };
