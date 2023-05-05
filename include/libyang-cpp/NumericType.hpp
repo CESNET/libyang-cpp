CESNET/libyang-cpp

commit 7565fe3b010a132bcacb3c7524a3c9fe4031558e

refactor: forward-declare the Value variant
...because it will be used for numeric limits within Types.h which is
already included from Value.h.

Change-Id: I5a298d43b9f2bca86cbb7f7a840b3b27004f0a4f

include/libyang-cpp/Value.hpp
@@ -9,6 +9,7 @@
 #pragma once
 #include <cstdint>
 #include <libyang-cpp/Type.hpp>
+#include <libyang-cpp/ValueFwd.hpp>
 #include <libyang-cpp/export.h>
 #include <optional>
 #include <string>
@@ -212,31 +213,6 @@ static_assert(456.789_decimal64 == Decimal64::fromRawDecimal<3>(456789));
 static_assert(456.7890_decimal64 == Decimal64::fromRawDecimal<4>(4567890));
 static_assert(-456.7890_decimal64 == Decimal64::fromRawDecimal<4>(-4567890));
 }
-
-class DataNode;
-
-/**
- * Represents a value of DataNodeTerm.
- */
-using Value = std::variant<
-    int8_t,
-    int16_t,
-    int32_t,
-    int64_t,
-    uint8_t,
-    uint16_t,
-    uint32_t,
-    uint64_t,
-    bool,
-    Empty,
-    Binary,
-    std::string,
-    std::optional<DataNode>, // Instance identifier value.
-    Decimal64,
-    std::vector<Bit>,
-    Enum,
-    IdentityRef
->;
 
 /**
  * @brief A JSON value of an anydata node.

include/libyang-cpp/ValueFwd.hpp
@@ -1,0 +1,49 @@
+/*
+ * Copyright (C) 2021-2023 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Václav Kubernát <kubernat@cesnet.cz>
+ * Written by Jan Kundrát <jan.kundrat@cesnet.cz>
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+*/
+#pragma once
+#include <cstdint>
+#include <libyang-cpp/export.h>
+#include <optional>
+#include <string>
+#include <variant>
+#include <vector>
+
+namespace libyang {
+struct Empty;
+struct Binary;
+struct Bit;
+struct Enum;
+struct IdentityRef;
+struct Decimal64;
+class DataNode;
+
+/**
+ * Represents a value of DataNodeTerm.
+ */
+using Value = std::variant<
+    int8_t,
+    int16_t,
+    int32_t,
+    int64_t,
+    uint8_t,
+    uint16_t,
+    uint32_t,
+    uint64_t,
+    bool,
+    Empty,
+    Binary,
+    std::string,
+    std::optional<DataNode>, // Instance identifier value.
+    Decimal64,
+    std::vector<Bit>,
+    Enum,
+    IdentityRef
+>;
+
+}

commit 50fd57c0f10c20b59dc9bf9ad613665248010e0c

Fix build failure with MSVC
Change-Id: I5af3ed43ed9097094b7acd63672211afe0574967
Fixes: 7565fe3 ("refactor: forward-declare the Value variant")
Fixes: 5861137 ("schema: accessing numeric data limits")

include/libyang-cpp/NumericType.hpp
@@ -1,0 +1,41 @@
+/*
+ * Copyright (C) 2021-2023 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Jan Kundrát <jan.kundrat@cesnet.cz>
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+*/
+#pragma once
+
+#include <libyang-cpp/Type.hpp>
+#include <libyang-cpp/Value.hpp>
+
+namespace libyang::types {
+/**
+ * @brief Info about numeric data types -- {u,}int{8,16,32,64} and decimal64
+ *
+ * Wraps `lysc_type_number` or `lysc_type_dec`.
+ */
+class LIBYANG_CPP_EXPORT Numeric : public Type {
+public:
+    /**
+     * @brief Contains information about the `range` statement
+     *
+     * Wraps `struct lysc_range` for numeric data types.
+     * */
+    struct LIBYANG_CPP_EXPORT Range {
+        using Part = std::pair<Value, Value>;
+        std::vector<Part> parts;
+        std::optional<std::string> description;
+        std::optional<std::string> errorAppTag;
+        std::optional<std::string> errorMessage;
+    };
+
+    Range range() const;
+    uint8_t fractionDigits() const;
+
+    friend Type;
+private:
+    using Type::Type;
+};
+}

include/libyang-cpp/Type.hpp
@@ -8,7 +8,6 @@
 #pragma once
 
 #include <libyang-cpp/Enum.hpp>
-#include <libyang-cpp/ValueFwd.hpp>
 #include <libyang-cpp/export.h>
 #include <memory>
 #include <optional>
@@ -250,34 +249,6 @@ public:
 
     friend Type;
 
-private:
-    using Type::Type;
-};
-
-/**
- * @brief Info about numeric data types -- {u,}int{8,16,32,64} and decimal64
- *
- * Wraps `lysc_type_number` or `lysc_type_dec`.
- */
-class LIBYANG_CPP_EXPORT Numeric : public Type {
-public:
-    /**
-     * @brief Contains information about the `range` statement
-     *
-     * Wraps `struct lysc_range` for numeric data types.
-     * */
-    struct LIBYANG_CPP_EXPORT Range {
-        using Part = std::pair<Value, Value>;
-        std::vector<Part> parts;
-        std::optional<std::string> description;
-        std::optional<std::string> errorAppTag;
-        std::optional<std::string> errorMessage;
-    };
-
-    Range range() const;
-    uint8_t fractionDigits() const;
-
-    friend Type;
 private:
     using Type::Type;
 };

include/libyang-cpp/Value.hpp
@@ -213,6 +213,31 @@ static_assert(456.7890_decimal64 == Decimal64::fromRawDecimal<4>(4567890));
 static_assert(-456.7890_decimal64 == Decimal64::fromRawDecimal<4>(-4567890));
 }
 
+class DataNode;
+
+/**
+ * Represents a value of DataNodeTerm.
+ */
+using Value = std::variant<
+    int8_t,
+    int16_t,
+    int32_t,
+    int64_t,
+    uint8_t,
+    uint16_t,
+    uint32_t,
+    uint64_t,
+    bool,
+    Empty,
+    Binary,
+    std::string,
+    std::optional<DataNode>, // Instance identifier value.
+    Decimal64,
+    std::vector<Bit>,
+    Enum,
+    IdentityRef
+>;
+
 /**
  * @brief A JSON value of an anydata node.
  */

include/libyang-cpp/ValueFwd.hpp
@@ -1,49 +1,0 @@
-/*
- * Copyright (C) 2021-2023 CESNET, https://photonics.cesnet.cz/
- *
- * Written by Václav Kubernát <kubernat@cesnet.cz>
- * Written by Jan Kundrát <jan.kundrat@cesnet.cz>
- *
- * SPDX-License-Identifier: BSD-3-Clause
-*/
-#pragma once
-#include <cstdint>
-#include <libyang-cpp/export.h>
-#include <optional>
-#include <string>
-#include <variant>
-#include <vector>
-
-namespace libyang {
-struct Empty;
-struct Binary;
-struct Bit;
-struct Enum;
-struct IdentityRef;
-struct Decimal64;
-class DataNode;
-
-/**
- * Represents a value of DataNodeTerm.
- */
-using Value = std::variant<
-    int8_t,
-    int16_t,
-    int32_t,
-    int64_t,
-    uint8_t,
-    uint16_t,
-    uint32_t,
-    uint64_t,
-    bool,
-    Empty,
-    Binary,
-    std::string,
-    std::optional<DataNode>, // Instance identifier value.
-    Decimal64,
-    std::vector<Bit>,
-    Enum,
-    IdentityRef
->;
-
-}

src/Type.cpp
@@ -8,8 +8,8 @@
 
 #include <algorithm>
 #include <libyang-cpp/Module.hpp>
+#include <libyang-cpp/NumericType.hpp>
 #include <libyang-cpp/SchemaNode.hpp>
-#include <libyang-cpp/Type.hpp>
 #include <libyang-cpp/Utils.hpp>
 #include <libyang/libyang.h>
 #include <span>

tests/schema_node.cpp
@@ -8,6 +8,7 @@
 
 #include <doctest/doctest.h>
 #include <libyang-cpp/Context.hpp>
+#include <libyang-cpp/NumericType.hpp>
 #include <libyang-cpp/Utils.hpp>
 #include "example_schema.hpp"
 #include "pretty_printers.hpp"