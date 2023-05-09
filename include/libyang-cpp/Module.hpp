CESNET/libyang-cpp

commit 7565fe3b010a132bcacb3c7524a3c9fe4031558e

refactor: forward-declare the Value variant
...because it will be used for numeric limits within Types.h which is
already included from Value.h.

Change-Id: I5a298d43b9f2bca86cbb7f7a840b3b27004f0a4f

include/libyang-cpp/Value.hpp
@@ -9,6 +9,7 @@
 #pragma once
 #include <cstdint>
 #include <libyang-cpp/Type.hpp>
+#include <libyang-cpp/ValueFwd.hpp>
 #include <libyang-cpp/export.h>
 #include <optional>
 #include <string>
@@ -212,31 +213,6 @@ static_assert(456.789_decimal64 == Decimal64::fromRawDecimal<3>(456789));
 static_assert(456.7890_decimal64 == Decimal64::fromRawDecimal<4>(4567890));
 static_assert(-456.7890_decimal64 == Decimal64::fromRawDecimal<4>(-4567890));
 }
-
-class DataNode;
-
-/**
- * Represents a value of DataNodeTerm.
- */
-using Value = std::variant<
-    int8_t,
-    int16_t,
-    int32_t,
-    int64_t,
-    uint8_t,
-    uint16_t,
-    uint32_t,
-    uint64_t,
-    bool,
-    Empty,
-    Binary,
-    std::string,
-    std::optional<DataNode>, // Instance identifier value.
-    Decimal64,
-    std::vector<Bit>,
-    Enum,
-    IdentityRef
->;
 
 /**
  * @brief A JSON value of an anydata node.

include/libyang-cpp/ValueFwd.hpp
@@ -1,0 +1,49 @@
+/*
+ * Copyright (C) 2021-2023 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Václav Kubernát <kubernat@cesnet.cz>
+ * Written by Jan Kundrát <jan.kundrat@cesnet.cz>
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+*/
+#pragma once
+#include <cstdint>
+#include <libyang-cpp/export.h>
+#include <optional>
+#include <string>
+#include <variant>
+#include <vector>
+
+namespace libyang {
+struct Empty;
+struct Binary;
+struct Bit;
+struct Enum;
+struct IdentityRef;
+struct Decimal64;
+class DataNode;
+
+/**
+ * Represents a value of DataNodeTerm.
+ */
+using Value = std::variant<
+    int8_t,
+    int16_t,
+    int32_t,
+    int64_t,
+    uint8_t,
+    uint16_t,
+    uint32_t,
+    uint64_t,
+    bool,
+    Empty,
+    Binary,
+    std::string,
+    std::optional<DataNode>, // Instance identifier value.
+    Decimal64,
+    std::vector<Bit>,
+    Enum,
+    IdentityRef
+>;
+
+}

commit 2aa825dfd4bd203c7ef65dba9a3dd2760b5f788e

Avoid having to forward-declare std::variant<T>
The recent patches which added support for reporting numeric
ranges/limits of values were hit by a risk of a circular #include loop;
the std::variant that we use for reporting literal values (`Value`) was
needed from inside the `Type.hpp`. Simply forward-declaring the `Value`
broke the build on MSVC.

Since the only part that is needed is actually the `Identity` class, and
since that class is *not* something that's specific to types, but it's a
module-level statement, fix stuff by moving the `Identity` into
`Module.hpp`.

Change-Id: I5af3ed43ed9097094b7acd63672211afe0574967
Fixes: 7565fe3 ("refactor: forward-declare the Value variant")
Fixes: 5861137 ("schema: accessing numeric data limits")

include/libyang-cpp/Module.hpp
@@ -15,16 +15,22 @@
 
 struct ly_ctx;
 struct lys_module;
+struct lysc_ident;
 struct lysp_feature;
 
 namespace libyang {
 class Context;
 class DataNode;
+class DataNodeTerm;
 class Meta;
 class Module;
 class ChildInstanstiables;
 class Identity;
 class SchemaNode;
+
+namespace types {
+class IdentityRef;
+}
 
 /**
  * @brief Represents a feature of a module.
@@ -82,5 +88,29 @@ private:
 
     std::shared_ptr<ly_ctx> m_ctx;
     lys_module* m_module;
+};
+
+/**
+ * @brief Contains information about an identity.
+ *
+ * Wraps `lysc_ident`.
+ */
+class LIBYANG_CPP_EXPORT Identity {
+public:
+    friend DataNodeTerm;
+    friend Module;
+    friend types::IdentityRef;
+    std::vector<Identity> derived() const;
+    std::vector<Identity> derivedRecursive() const;
+    Module module() const;
+    std::string_view name() const;
+
+    bool operator==(const Identity& other) const;
+
+private:
+    Identity(const lysc_ident* ident, std::shared_ptr<ly_ctx> ctx);
+
+    const lysc_ident* m_ident;
+    std::shared_ptr<ly_ctx> m_ctx;
 };
 }

include/libyang-cpp/Type.hpp
@@ -8,7 +8,7 @@
 #pragma once
 
 #include <libyang-cpp/Enum.hpp>
-#include <libyang-cpp/ValueFwd.hpp>
+#include <libyang-cpp/Value.hpp>
 #include <libyang-cpp/export.h>
 #include <memory>
 #include <optional>
@@ -18,7 +18,6 @@
 #include <vector>
 
 struct ly_ctx;
-struct lysc_ident;
 struct lysc_type;
 struct lysp_type;
 
@@ -27,6 +26,7 @@ class DataNodeTerm;
 class Leaf;
 class LeafList;
 class Module;
+
 /**
  * @brief Contains representations of `leaf` schema data types.
  *
@@ -45,6 +45,7 @@ namespace constraints {
 using ListSize = uint32_t;
 }
 }
+
 /**
  * @brief Contains information about a leaf's type.
  *
@@ -79,30 +80,6 @@ protected:
 
 private:
     Type(const lysc_type* type, const lysp_type* typeParsed, std::shared_ptr<ly_ctx> ctx);
-};
-
-/**
- * @brief Contains information about an identity.
- *
- * Wraps `lysc_ident`.
- */
-class LIBYANG_CPP_EXPORT Identity {
-public:
-    friend DataNodeTerm;
-    friend Module;
-    friend types::IdentityRef;
-    std::vector<Identity> derived() const;
-    std::vector<Identity> derivedRecursive() const;
-    Module module() const;
-    std::string_view name() const;
-
-    bool operator==(const Identity& other) const;
-
-private:
-    Identity(const lysc_ident* ident, std::shared_ptr<ly_ctx> ctx);
-
-    const lysc_ident* m_ident;
-    std::shared_ptr<ly_ctx> m_ctx;
 };
 
 namespace types {

include/libyang-cpp/Value.hpp
@@ -8,7 +8,7 @@
 */
 #pragma once
 #include <cstdint>
-#include <libyang-cpp/Type.hpp>
+#include <libyang-cpp/Module.hpp>
 #include <libyang-cpp/export.h>
 #include <optional>
 #include <string>
@@ -212,6 +212,31 @@ static_assert(456.789_decimal64 == Decimal64::fromRawDecimal<3>(456789));
 static_assert(456.7890_decimal64 == Decimal64::fromRawDecimal<4>(4567890));
 static_assert(-456.7890_decimal64 == Decimal64::fromRawDecimal<4>(-4567890));
 }
+
+class DataNode;
+
+/**
+ * Represents a value of DataNodeTerm.
+ */
+using Value = std::variant<
+    int8_t,
+    int16_t,
+    int32_t,
+    int64_t,
+    uint8_t,
+    uint16_t,
+    uint32_t,
+    uint64_t,
+    bool,
+    Empty,
+    Binary,
+    std::string,
+    std::optional<DataNode>, // Instance identifier value.
+    Decimal64,
+    std::vector<Bit>,
+    Enum,
+    IdentityRef
+>;
 
 /**
  * @brief A JSON value of an anydata node.

include/libyang-cpp/ValueFwd.hpp
@@ -1,49 +1,0 @@
-/*
- * Copyright (C) 2021-2023 CESNET, https://photonics.cesnet.cz/
- *
- * Written by Václav Kubernát <kubernat@cesnet.cz>
- * Written by Jan Kundrát <jan.kundrat@cesnet.cz>
- *
- * SPDX-License-Identifier: BSD-3-Clause
-*/
-#pragma once
-#include <cstdint>
-#include <libyang-cpp/export.h>
-#include <optional>
-#include <string>
-#include <variant>
-#include <vector>
-
-namespace libyang {
-struct Empty;
-struct Binary;
-struct Bit;
-struct Enum;
-struct IdentityRef;
-struct Decimal64;
-class DataNode;
-
-/**
- * Represents a value of DataNodeTerm.
- */
-using Value = std::variant<
-    int8_t,
-    int16_t,
-    int32_t,
-    int64_t,
-    uint8_t,
-    uint16_t,
-    uint32_t,
-    uint64_t,
-    bool,
-    Empty,
-    Binary,
-    std::string,
-    std::optional<DataNode>, // Instance identifier value.
-    Decimal64,
-    std::vector<Bit>,
-    Enum,
-    IdentityRef
->;
-
-}

src/Module.cpp
@@ -12,6 +12,7 @@
 #include <libyang-cpp/Utils.hpp>
 #include <libyang/libyang.h>
 #include <span>
+#include <stack>
 #include "utils/exception.hpp"
 
 namespace libyang {
@@ -176,5 +177,73 @@ Feature::Feature(const lysp_feature* feature, std::shared_ptr<ly_ctx> ctx)
 std::string_view Feature::name() const
 {
     return m_feature->name;
+}
+
+Identity::Identity(const lysc_ident* ident, std::shared_ptr<ly_ctx> ctx)
+    : m_ident(ident)
+    , m_ctx(ctx)
+{
+}
+
+/**
+ * @brief Returns the derived identities of this identity non-recursively.
+ *
+ * Wraps `lysc_ident::dereived`
+ */
+std::vector<Identity> Identity::derived() const
+{
+    std::vector<Identity> res;
+    for (const auto& it : std::span(m_ident->derived, LY_ARRAY_COUNT(m_ident->derived))) {
+        res.emplace_back(Identity{it, m_ctx});
+    }
+
+    return res;
+}
+
+/**
+ * @brief Returns the derived identities of this identity recursively.
+ */
+std::vector<Identity> Identity::derivedRecursive() const
+{
+    std::stack<libyang::Identity> stack({*this});
+    std::set<libyang::Identity, SomeOrder> visited({*this});
+
+    while (!stack.empty()) {
+        auto currentIdentity = std::move(stack.top());
+        stack.pop();
+
+        for (const auto& derived : currentIdentity.derived()) {
+            if (auto ins = visited.insert(derived); ins.second) {
+                stack.push(derived);
+            }
+        }
+    }
+
+    return {visited.begin(), visited.end()};
+}
+
+/**
+ * @brief Returns the module of the identity.
+ *
+ * Wraps `lysc_ident::module`
+ */
+Module Identity::module() const
+{
+    return Module{m_ident->module, m_ctx};
+}
+
+/**
+ * @brief Returns the name of the identity.
+ *
+ * Wraps `lysc_ident::name`
+ */
+std::string_view Identity::name() const
+{
+    return m_ident->name;
+}
+
+bool Identity::operator==(const Identity& other) const
+{
+    return module().name() == other.module().name() && name() == other.name();
 }
 }

src/Type.cpp
@@ -7,13 +7,11 @@
 */
 
 #include <algorithm>
-#include <libyang-cpp/Module.hpp>
 #include <libyang-cpp/SchemaNode.hpp>
 #include <libyang-cpp/Type.hpp>
 #include <libyang-cpp/Utils.hpp>
 #include <libyang/libyang.h>
 #include <span>
-#include <stack>
 #include "utils/enum.hpp"
 
 namespace libyang {
@@ -224,74 +222,6 @@ std::vector<Identity> types::IdentityRef::bases() const
     }
 
     return res;
-}
-
-Identity::Identity(const lysc_ident* ident, std::shared_ptr<ly_ctx> ctx)
-    : m_ident(ident)
-    , m_ctx(ctx)
-{
-}
-
-/**
- * @brief Returns the derived identities of this identity non-recursively.
- *
- * Wraps `lysc_ident::dereived`
- */
-std::vector<Identity> Identity::derived() const
-{
-    std::vector<Identity> res;
-    for (const auto& it : std::span(m_ident->derived, LY_ARRAY_COUNT(m_ident->derived))) {
-        res.emplace_back(Identity{it, m_ctx});
-    }
-
-    return res;
-}
-
-/**
- * @brief Returns the derived identities of this identity recursively.
- */
-std::vector<Identity> Identity::derivedRecursive() const
-{
-    std::stack<libyang::Identity> stack({*this});
-    std::set<libyang::Identity, SomeOrder> visited({*this});
-
-    while (!stack.empty()) {
-        auto currentIdentity = std::move(stack.top());
-        stack.pop();
-
-        for (const auto& derived : currentIdentity.derived()) {
-            if (auto ins = visited.insert(derived); ins.second) {
-                stack.push(derived);
-            }
-        }
-    }
-
-    return {visited.begin(), visited.end()};
-}
-
-/**
- * @brief Returns the module of the identity.
- *
- * Wraps `lysc_ident::module`
- */
-Module Identity::module() const
-{
-    return Module{m_ident->module, m_ctx};
-}
-
-/**
- * @brief Returns the name of the identity.
- *
- * Wraps `lysc_ident::name`
- */
-std::string_view Identity::name() const
-{
-    return m_ident->name;
-}
-
-bool Identity::operator==(const Identity& other) const
-{
-    return module().name() == other.module().name() && name() == other.name();
 }
 
 /**

tests/schema_node.cpp
@@ -8,6 +8,7 @@
 
 #include <doctest/doctest.h>
 #include <libyang-cpp/Context.hpp>
+#include <libyang-cpp/Type.hpp>
 #include <libyang-cpp/Utils.hpp>
 #include "example_schema.hpp"
 #include "pretty_printers.hpp"