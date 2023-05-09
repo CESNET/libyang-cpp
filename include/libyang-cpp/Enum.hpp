CESNET/libyang-cpp

commit 510d5ac5fe5f4da4acd191b83c4ba7e17aea344d

Fix symbol export/visibility
Turns out that this macro is needed also on Mac OS X builds (both with
GCC 11.3 for x86_64 and with Apple clang version 14.0.0
(clang-1400.0.29.202) for arm64-apple-darwin22.1.0). If I'm reading my
original commit message correctly, this was only choking on MSVC, so
let's use that macro everywhere but on MSVC.

Fixes: 1d7c822 Make this buildable on Ubuntu 20.04 with GCC 10
Bug: https://github.com/CESNET/libyang-cpp/issues/7
Change-Id: Ib3502057d17ab8d4a138de02204d0b8e1e1c6442

include/libyang-cpp/Enum.hpp
@@ -292,12 +292,12 @@ constexpr ParseOptions operator|(const ParseOptions a, const ParseOptions b)
 }
 
 std::ostream& operator<<(std::ostream& os, const NodeType& type)
-#if defined(__GNUC__) && !defined(__llvm__) && __GNUC__ <= 10
+#if !defined(_MSC_VER)
     LIBYANG_CPP_EXPORT
 #endif
     ;
 std::ostream& operator<<(std::ostream& os, const ErrorCode& err)
-#if defined(__GNUC__) && !defined(__llvm__) && __GNUC__ <= 10
+#if !defined(_MSC_VER)
     LIBYANG_CPP_EXPORT
 #endif
     ;

commit 6c3d020aeeb588dfb886d93815718b4500a2977b

Fix the import/export mess
Turns out that this symbol is actually needed for all builds because
it's used in the test suite already. Besides, it's meant as a public
function, so it really has to be made available to everybody.

Fixes: 510d5ac ("Fix symbol export/visibility")
Change-Id: I2a6017eaca61c0bc1a536cdd877b3404373a70ec

include/libyang-cpp/Enum.hpp
@@ -291,14 +291,6 @@ constexpr ParseOptions operator|(const ParseOptions a, const ParseOptions b)
     return implEnumBitOr(a, b);
 }
 
-std::ostream& operator<<(std::ostream& os, const NodeType& type)
-#if !defined(_MSC_VER)
-    LIBYANG_CPP_EXPORT
-#endif
-    ;
-std::ostream& operator<<(std::ostream& os, const ErrorCode& err)
-#if !defined(_MSC_VER)
-    LIBYANG_CPP_EXPORT
-#endif
-    ;
+std::ostream& LIBYANG_CPP_EXPORT operator<<(std::ostream& os, const NodeType& type);
+std::ostream& LIBYANG_CPP_EXPORT operator<<(std::ostream& os, const ErrorCode& err);
 }