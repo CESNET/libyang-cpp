CESNET/libyang-cpp

commit fb60895835047d56c5fff4cfff42a46c92338ee2

Allow getting module extensions
Wraps lysc_ext and lysc_ext_instance.

Change-Id: I28b7075f7b44fcb68690f071e16808e31e29d486

src/Module.cpp
@@ -15,6 +15,8 @@
 #include <stack>
 #include "utils/exception.hpp"
 
+using namespace std::string_literals;
+
 namespace libyang {
 Module::Module(lys_module* module, std::shared_ptr<ly_ctx> ctx)
     : m_ctx(ctx)
@@ -163,6 +165,28 @@ ChildInstanstiables Module::childInstantiables() const
     return ChildInstanstiables{nullptr, m_module->compiled, m_ctx};
 }
 
+std::vector<ExtensionInstance> Module::extensionInstances() const
+{
+    std::vector<ExtensionInstance> res;
+    auto span = std::span<lysc_ext_instance>(m_module->compiled->exts, LY_ARRAY_COUNT(m_module->compiled->exts));
+    std::transform(span.begin(), span.end(), std::back_inserter(res), [this] (const lysc_ext_instance& ext) {
+        return ExtensionInstance(&ext, m_ctx);
+    });
+    return res;
+}
+
+ExtensionInstance Module::extensionInstance(const std::string& name) const
+{
+    auto span = std::span<lysc_ext_instance>(m_module->compiled->exts, LY_ARRAY_COUNT(m_module->compiled->exts));
+    auto it = std::find_if(span.begin(), span.end(), [name](const auto& ext) {
+        return ext.argument == name;
+    });
+    if (it == span.end()) {
+        throw Error{"Extension \""s + name + "\" not defined in module \"" + std::string{this->name()} + "\""};
+    }
+    return ExtensionInstance(&*it, m_ctx);
+}
+
 Feature::Feature(const lysp_feature* feature, std::shared_ptr<ly_ctx> ctx)
     : m_feature(feature)
     , m_ctx(ctx)
@@ -245,5 +269,47 @@ std::string_view Identity::name() const
 bool Identity::operator==(const Identity& other) const
 {
     return module().name() == other.module().name() && name() == other.name();
+}
+
+ExtensionInstance::ExtensionInstance(const lysc_ext_instance* ext, std::shared_ptr<ly_ctx> ctx)
+    : m_ext(ext)
+    , m_ctx(ctx)
+{
+}
+
+/**
+ * @brief Returns the argument
+ *
+ * Wraps `lysc_ext_instance::argument`.
+ */
+std::string_view ExtensionInstance::argument() const
+{
+    return m_ext->argument;
+}
+
+/**
+ * @brief Returns the extension definition
+ *
+ * Wraps `lysc_ext_instance::name`.
+ */
+Extension ExtensionInstance::definition() const
+{
+    return Extension{m_ext->def, m_ctx};
+}
+
+Extension::Extension(const lysc_ext* ext, std::shared_ptr<ly_ctx> ctx)
+    : m_ext(ext)
+    , m_ctx(ctx)
+{
+}
+
+/**
+ * @brief Returns the name of the module.
+ *
+ * Wraps `lysc_ext::name`.
+ */
+std::string_view Extension::name() const
+{
+    return m_ext->name;
 }
 }

include/libyang-cpp/Module.hpp
@@ -16,12 +16,16 @@
 struct ly_ctx;
 struct lys_module;
 struct lysc_ident;
+struct lysc_ext;
+struct lysc_ext_instance;
 struct lysp_feature;
 
 namespace libyang {
 class Context;
 class DataNode;
 class DataNodeTerm;
+class Extension;
+class ExtensionInstance;
 class Meta;
 class Module;
 class ChildInstanstiables;
@@ -69,6 +73,8 @@ public:
     bool implemented() const;
     bool featureEnabled(const std::string& featureName) const;
     std::vector<Feature> features() const;
+    std::vector<ExtensionInstance> extensionInstances() const;
+    ExtensionInstance extensionInstance(const std::string& name) const;
     void setImplemented();
     void setImplemented(std::vector<std::string> features);
     void setImplemented(const AllFeatures);
@@ -112,5 +118,42 @@ private:
 
     const lysc_ident* m_ident;
     std::shared_ptr<ly_ctx> m_ctx;
+};
+
+/**
+ * @brief Contains information about compiled extension.
+ *
+ * Wraps `lysc_extension_instance`
+ */
+class LIBYANG_CPP_EXPORT ExtensionInstance {
+public:
+    Extension definition() const;
+    std::string_view argument() const;
+
+private:
+    ExtensionInstance(const lysc_ext_instance* ext, std::shared_ptr<ly_ctx> ctx);
+
+    const lysc_ext_instance* m_ext;
+    std::shared_ptr<ly_ctx> m_ctx;
+
+    friend Module;
+};
+
+/**
+ * @brief Contains information about an extension definition.
+ *
+ * Wraps `lysc_ext`
+ */
+class LIBYANG_CPP_EXPORT Extension {
+public:
+    std::string_view name() const;
+
+private:
+    Extension(const lysc_ext* def, std::shared_ptr<ly_ctx> ctx);
+
+    const lysc_ext* m_ext;
+    std::shared_ptr<ly_ctx> m_ctx;
+
+    friend ExtensionInstance;
 };
 }

tests/context.cpp
@@ -120,6 +120,27 @@ TEST_CASE("context")
         }
     }
 
+    DOCTEST_SUBCASE("Get module extensions")
+    {
+        ctx->setSearchDir(TESTS_DIR);
+        auto mod = ctx->loadModule("ietf-restconf", std::nullopt);
+
+        REQUIRE(mod.name() == "ietf-restconf");
+        REQUIRE(mod.extensionInstances().size() == 2);
+
+        REQUIRE(mod.extensionInstances()[0].argument() == "yang-errors");
+        REQUIRE(mod.extensionInstances()[0].definition().name() == "yang-data");
+        REQUIRE(mod.extensionInstance("yang-errors").argument() == "yang-errors");
+        REQUIRE(mod.extensionInstance("yang-errors").definition().name() == "yang-data");
+
+        REQUIRE(mod.extensionInstances()[1].argument() == "yang-api");
+        REQUIRE(mod.extensionInstances()[1].definition().name() == "yang-data");
+        REQUIRE(mod.extensionInstance("yang-api").argument() == "yang-api");
+        REQUIRE(mod.extensionInstance("yang-api").definition().name() == "yang-data");
+
+        REQUIRE_THROWS_WITH_AS(mod.extensionInstance("yay"), "Extension \"yay\" not defined in module \"ietf-restconf\"", libyang::Error);
+    }
+
     DOCTEST_SUBCASE("context lifetime")
     {
         ctx->parseModule(valid_yang_model, libyang::SchemaFormat::YANG);

tests/ietf-restconf@2017-01-26.yang
@@ -1,0 +1,278 @@
+module ietf-restconf {
+  yang-version 1.1;
+  namespace "urn:ietf:params:xml:ns:yang:ietf-restconf";
+  prefix "rc";
+
+  organization
+    "IETF NETCONF (Network Configuration) Working Group";
+
+  contact
+    "WG Web:   <https://datatracker.ietf.org/wg/netconf/>
+     WG List:  <mailto:netconf@ietf.org>
+
+     Author:   Andy Bierman
+               <mailto:andy@yumaworks.com>
+
+     Author:   Martin Bjorklund
+               <mailto:mbj@tail-f.com>
+
+     Author:   Kent Watsen
+               <mailto:kwatsen@juniper.net>";
+
+  description
+    "This module contains conceptual YANG specifications
+     for basic RESTCONF media type definitions used in
+     RESTCONF protocol messages.
+
+     Note that the YANG definitions within this module do not
+     represent configuration data of any kind.
+     The 'restconf-media-type' YANG extension statement
+     provides a normative syntax for XML and JSON
+     message-encoding purposes.
+
+     Copyright (c) 2017 IETF Trust and the persons identified as
+     authors of the code.  All rights reserved.
+
+     Redistribution and use in source and binary forms, with or
+     without modification, is permitted pursuant to, and subject
+     to the license terms contained in, the Simplified BSD License
+     set forth in Section 4.c of the IETF Trust's Legal Provisions
+     Relating to IETF Documents
+     (http://trustee.ietf.org/license-info).
+
+     This version of this YANG module is part of RFC 8040; see
+     the RFC itself for full legal notices.";
+
+  revision 2017-01-26 {
+    description
+      "Initial revision.";
+    reference
+      "RFC 8040: RESTCONF Protocol.";
+  }
+
+  extension yang-data {
+    argument name {
+      yin-element true;
+    }
+    description
+      "This extension is used to specify a YANG data template that
+       represents conceptual data defined in YANG.  It is
+       intended to describe hierarchical data independent of
+       protocol context or specific message-encoding format.
+       Data definition statements within a yang-data extension
+       specify the generic syntax for the specific YANG data
+       template, whose name is the argument of the 'yang-data'
+       extension statement.
+
+       Note that this extension does not define a media type.
+       A specification using this extension MUST specify the
+       message-encoding rules, including the content media type.
+
+       The mandatory 'name' parameter value identifies the YANG
+       data template that is being defined.  It contains the
+       template name.
+
+       This extension is ignored unless it appears as a top-level
+       statement.  It MUST contain data definition statements
+       that result in exactly one container data node definition.
+       An instance of a YANG data template can thus be translated
+       into an XML instance document, whose top-level element
+       corresponds to the top-level container.
+       The module name and namespace values for the YANG module using
+       the extension statement are assigned to instance document data
+       conforming to the data definition statements within
+       this extension.
+
+       The substatements of this extension MUST follow the
+       'data-def-stmt' rule in the YANG ABNF.
+
+       The XPath document root is the extension statement itself,
+       such that the child nodes of the document root are
+       represented by the data-def-stmt substatements within
+       this extension.  This conceptual document is the context
+       for the following YANG statements:
+
+         - must-stmt
+         - when-stmt
+         - path-stmt
+         - min-elements-stmt
+         - max-elements-stmt
+         - mandatory-stmt
+         - unique-stmt
+         - ordered-by
+         - instance-identifier data type
+
+       The following data-def-stmt substatements are constrained
+       when used within a 'yang-data' extension statement.
+
+         - The list-stmt is not required to have a key-stmt defined.
+         - The if-feature-stmt is ignored if present.
+         - The config-stmt is ignored if present.
+         - The available identity values for any 'identityref'
+           leaf or leaf-list nodes are limited to the module
+           containing this extension statement and the modules
+           imported into that module.
+      ";
+  }
+
+  rc:yang-data yang-errors {
+    uses errors;
+  }
+
+  rc:yang-data yang-api {
+    uses restconf;
+  }
+
+  grouping errors {
+    description
+      "A grouping that contains a YANG container
+       representing the syntax and semantics of a
+       YANG Patch error report within a response message.";
+
+    container errors {
+      description
+        "Represents an error report returned by the server if
+         a request results in an error.";
+
+      list error {
+        description
+          "An entry containing information about one
+           specific error that occurred while processing
+           a RESTCONF request.";
+        reference
+          "RFC 6241, Section 4.3.";
+
+        leaf error-type {
+          type enumeration {
+            enum transport {
+              description
+                "The transport layer.";
+            }
+            enum rpc {
+              description
+                "The rpc or notification layer.";
+            }
+            enum protocol {
+              description
+                "The protocol operation layer.";
+            }
+            enum application {
+              description
+                "The server application layer.";
+            }
+          }
+          mandatory true;
+          description
+            "The protocol layer where the error occurred.";
+        }
+
+        leaf error-tag {
+          type string;
+          mandatory true;
+          description
+            "The enumerated error-tag.";
+        }
+
+        leaf error-app-tag {
+          type string;
+          description
+            "The application-specific error-tag.";
+        }
+
+        leaf error-path {
+          type instance-identifier;
+          description
+            "The YANG instance identifier associated
+             with the error node.";
+        }
+
+        leaf error-message {
+          type string;
+          description
+            "A message describing the error.";
+        }
+
+        anydata error-info {
+           description
+             "This anydata value MUST represent a container with
+              zero or more data nodes representing additional
+              error information.";
+        }
+      }
+    }
+  }
+
+  grouping restconf {
+    description
+      "Conceptual grouping representing the RESTCONF
+       root resource.";
+
+    container restconf {
+      description
+        "Conceptual container representing the RESTCONF
+         root resource.";
+
+      container data {
+        description
+          "Container representing the datastore resource.
+           Represents the conceptual root of all state data
+           and configuration data supported by the server.
+           The child nodes of this container can be any data
+           resources that are defined as top-level data nodes
+           from the YANG modules advertised by the server in
+           the 'ietf-yang-library' module.";
+      }
+
+      container operations {
+        description
+          "Container for all operation resources.
+
+           Each resource is represented as an empty leaf with the
+           name of the RPC operation from the YANG 'rpc' statement.
+
+           For example, the 'system-restart' RPC operation defined
+           in the 'ietf-system' module would be represented as
+           an empty leaf in the 'ietf-system' namespace.  This is
+           a conceptual leaf and will not actually be found in
+           the module:
+
+              module ietf-system {
+                leaf system-reset {
+                  type empty;
+                }
+              }
+
+           To invoke the 'system-restart' RPC operation:
+
+              POST /restconf/operations/ietf-system:system-restart
+
+           To discover the RPC operations supported by the server:
+
+              GET /restconf/operations
+
+           In XML, the YANG module namespace identifies the module:
+
+             <system-restart
+                xmlns='urn:ietf:params:xml:ns:yang:ietf-system'/>
+
+           In JSON, the YANG module name identifies the module:
+
+             { 'ietf-system:system-restart' : [null] }
+          ";
+      }
+      leaf yang-library-version {
+        type string {
+          pattern '\d{4}-\d{2}-\d{2}';
+        }
+        config false;
+        mandatory true;
+        description
+          "Identifies the revision date of the 'ietf-yang-library'
+           module that is implemented by this RESTCONF server.
+           Indicates the year, month, and day in YYYY-MM-DD
+           numeric format.";
+      }
+    }
+  }
+
+}

commit a05b921a440008baee5cef960fdfbc17c3852900

Fix nullptr dereference when fetching module extensions
In commit fb60895 ("Allow getting module extensions") we implemented
getting extensions from YANG modules. However, in case the module was
not implemented, then the could crash on nullptr access. The libyang
docs for `lys_module` clearly say that the `compiled` field is
available only for implemented modules.

Fixes: fb60895835047d56c5fff4cfff42a46c92338ee2
Change-Id: I766e1c1267b304256049557bd4b5f3e056171d13

src/Module.cpp
@@ -167,6 +167,10 @@ ChildInstanstiables Module::childInstantiables() const
 
 std::vector<ExtensionInstance> Module::extensionInstances() const
 {
+    if (!m_module->compiled) {
+        throw Error{"Module \"" + std::string{this->name()} + "\" not implemented"};
+    }
+
     std::vector<ExtensionInstance> res;
     auto span = std::span<lysc_ext_instance>(m_module->compiled->exts, LY_ARRAY_COUNT(m_module->compiled->exts));
     std::transform(span.begin(), span.end(), std::back_inserter(res), [this] (const lysc_ext_instance& ext) {
@@ -177,6 +181,10 @@ std::vector<ExtensionInstance> Module::extensionInstances() const
 
 ExtensionInstance Module::extensionInstance(const std::string& name) const
 {
+    if (!m_module->compiled) {
+        throw Error{"Module \"" + std::string{this->name()} + "\" not implemented"};
+    }
+
     auto span = std::span<lysc_ext_instance>(m_module->compiled->exts, LY_ARRAY_COUNT(m_module->compiled->exts));
     auto it = std::find_if(span.begin(), span.end(), [name](const auto& ext) {
         return ext.argument == name;