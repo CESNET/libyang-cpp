CESNET/libyang-cpp

commit 364deb538221e9881253ed8947eb2c05b39f427e

context: requesting "the latest version of this module"
Or "any version, I don't care at all", which is how this was used in
netconf-cli, and how I discovered the bug fixed in the previous commit
in the first place.

Change-Id: I53474098ae169f568e12e1c5325fee910ce76f6c

src/Context.cpp
@@ -435,6 +435,25 @@ std::optional<Module> Context::getModuleImplemented(const std::string& name) con
 }
 
 /**
+ * @brief Retrieves the latest version of a module from the context.
+ *
+ * Wraps `ly_ctx_get_module_latest`.
+ *
+ * @param name Name of the wanted module.
+ * @return The wanted module or std::nullopt if there is no module with the name.
+ */
+std::optional<Module> Context::getModuleLatest(const std::string& name) const
+{
+    auto mod = ly_ctx_get_module_latest(m_ctx.get(), name.c_str());
+
+    if (!mod) {
+        return std::nullopt;
+    }
+
+    return Module{mod, m_ctx};
+}
+
+/**
  * @brief Loads a module through its name and revision.
  *
  * @param name The name of te module to be loaded.

include/libyang-cpp/Context.hpp
@@ -95,6 +95,7 @@ public:
     void setSearchDir(const std::filesystem::path& searchDir) const;
     std::optional<Module> getModule(const std::string& name, const std::optional<std::string>& revision) const;
     std::optional<Module> getModuleImplemented(const std::string& name) const;
+    std::optional<Module> getModuleLatest(const std::string& name) const;
     std::vector<Module> modules() const;
     void registerModuleCallback(std::function<ModuleCallback> callback);
 

tests/context.cpp
@@ -336,6 +336,9 @@ TEST_CASE("context")
 
         REQUIRE(ctx->getModuleImplemented("withImport").has_value());
         REQUIRE(!ctx->getModuleImplemented("importedModule").has_value());
+
+        REQUIRE(ctx->getModuleLatest("withImport"));
+        REQUIRE(ctx->getModuleLatest("importedModule"));
     }
 
     DOCTEST_SUBCASE("Context::parseData")

commit 58419dafd8a53d16f98aee6e511192764ed9b73f

docs: fix a typo
Fixes: 364deb5 (context: requesting "the latest version of this module")
Change-Id: Ia27e6c97022e098500b652c7e53988ef4b17e6e9

src/Context.cpp
@@ -456,7 +456,7 @@ std::optional<Module> Context::getModuleLatest(const std::string& name) const
 /**
  * @brief Loads a module through its name and revision.
  *
- * @param name The name of te module to be loaded.
+ * @param name The name of the module to be loaded.
  * @param revision Optional revision of the module to be loaded.
  * @param features Optional features array to be enabled. Pass {"*"} to enable all of them.
  *