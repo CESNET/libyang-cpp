CESNET/libyang-cpp

commit b9113aea9dec75270bb934509e89ba1946d3c98a

Implement value retrieval
Decimal64 implementation is imported from cla-sysrepo, commit
6a2b76cabe3d433c83a20bddf59449cdd8b130d2 with these changes:
- I made the ctor public.
- operator== is now default-declared (and operator!= is generated
  automatically in C++20)

Change-Id: I4c14fa0161cad651fcc7c299a3c0e56db510741d

src/DataNode.cpp
@@ -5,7 +5,10 @@
  *
  * SPDX-License-Identifier: BSD-3-Clause
 */
+#include <cstring>
+#include <functional>
 #include <libyang/libyang.h>
+#include <libyang/tree_data.h>
 #include <stdexcept>
 #include <string>
 #include <libyang-cpp/DataNode.hpp>
@@ -113,8 +116,120 @@ DataNodeTerm DataNode::asTerm() const
     return DataNodeTerm{m_node, m_viewCount};
 }
 
+/**
+ * Check if both operands point to the same node in the same tree.
+ */
+bool DataNode::operator==(const DataNode &node) const
+{
+    return this->m_node == node.m_node;
+}
+
 std::string_view DataNodeTerm::valueStr() const
 {
     return lyd_get_value(m_node);
+}
+
+namespace {
+/**
+ * This function emulates LYD_VALUE_GET. I can't use that macro directly, because it implicitly converts void* to Type*
+ * and that's not allowed in C++.
+ */
+template <typename Type>
+const Type* valueGetSpecial(const lyd_value* value)
+{
+    if (sizeof(Type) > LYD_VALUE_FIXED_MEM_SIZE) {
+        return static_cast<const Type*>(value->dyn_mem);
+    } else {
+        return reinterpret_cast<const Type*>(value->fixed_mem);
+    }
+}
+}
+
+/**
+ * Retrieves a value in a machine-readable format. The lifetime of the value is not connected to the lifetime of the
+ * original DataNodeTerm.
+ */
+Value DataNodeTerm::value() const
+{
+    std::function<Value(lyd_value)> impl = [this, &impl] (const lyd_value value) -> Value {
+        auto baseType = value.realtype->basetype;
+        switch (baseType) {
+        case LY_TYPE_INT8:
+            return value.int8;
+        case LY_TYPE_INT16:
+            return value.int16;
+        case LY_TYPE_INT32:
+            return value.int32;
+        case LY_TYPE_INT64:
+            return value.int64;
+        case LY_TYPE_UINT8:
+            return value.uint8;
+        case LY_TYPE_UINT16:
+            return value.uint16;
+        case LY_TYPE_UINT32:
+            return value.uint32;
+        case LY_TYPE_UINT64:
+            return value.uint64;
+        case LY_TYPE_BOOL:
+            return static_cast<bool>(value.boolean);
+        case LY_TYPE_EMPTY:
+            return Empty{};
+        case LY_TYPE_BINARY:
+        {
+            auto binValue = valueGetSpecial<lyd_value_binary>(&value);
+            Binary res;
+            std::copy(static_cast<uint8_t*>(binValue->data), static_cast<uint8_t*>(binValue->data) + binValue->size, std::back_inserter(res.data));
+            res.base64 = valueStr();
+            return res;
+        }
+        case LY_TYPE_STRING:
+            // valueStr gives a string_view, so here I have to copy the string.
+            return std::string(valueStr());
+        case LY_TYPE_UNION:
+            return impl(value.subvalue->value);
+        case LY_TYPE_DEC64:
+        {
+            auto schemaDef = reinterpret_cast<const lysc_node_leaf*>(m_node->schema);
+            auto dec = reinterpret_cast<const lysc_type_dec*>(schemaDef->type);
+            return Decimal64{value.dec64, dec->fraction_digits};
+        }
+        case LY_TYPE_BITS:
+        {
+            auto bits = valueGetSpecial<lyd_value_bits>(&value);
+            std::vector<Bit> res;
+            std::transform(bits->items, bits->items + LY_ARRAY_COUNT(bits->items), std::back_inserter(res), [] (const lysc_type_bitenum_item* bit) {
+                return Bit{.position = bit->position, .name = bit->name};
+            });
+
+            return res;
+        }
+        case LY_TYPE_ENUM:
+            return Enum{value.enum_item->name};
+        case LY_TYPE_IDENT:
+            return IdentityRef{.module = value.ident->module->name, .name = value.ident->name};
+        case LY_TYPE_INST:
+        {
+            lyd_node* out;
+            auto err = lyd_find_target(value.target, m_node, &out);
+            switch (err) {
+            case LY_SUCCESS:
+                return DataNode{out, m_viewCount};
+            case LY_ENOTFOUND:
+                return std::nullopt;
+            default:
+                throw ErrorWithCode("Error when finding inst-id target (" + std::to_string(err) + ")", err);
+            }
+        }
+        case LY_TYPE_LEAFREF:
+            // Leafrefs are resolved to the underlying types, so this should never happen.
+            throw std::logic_error("Unknown type");
+        case LY_TYPE_UNKNOWN:
+            throw Error("Unknown type");
+        }
+        __builtin_unreachable();
+    };
+
+
+    return impl(reinterpret_cast<const lyd_node_term*>(m_node)->value);
 }
 }

tests/data_node.cpp
@@ -7,8 +7,82 @@
 */
 
 #include <doctest/doctest.h>
+#include <experimental/iterator>
 #include <libyang-cpp/Context.hpp>
+#include <sstream>
 
+using namespace std::string_literals;
+
+namespace libyang {
+    namespace {
+        struct impl_toStruct {
+            std::string operator()(const Binary& value)
+            {
+                std::ostringstream oss;
+                oss << std::hex;
+                std::transform(value.data.begin(), value.data.end(), std::experimental::make_ostream_joiner(oss, ", "), [] (uint8_t num) {
+                    return std::to_string(static_cast<unsigned int>(num));
+                });
+                return oss.str();
+            }
+
+            std::string operator()(const Empty)
+            {
+                return "<empty>";
+            }
+
+            std::string operator()(const std::string& value)
+            {
+                return value;
+            }
+
+            std::string operator()(const std::optional<DataNode>& value)
+            {
+                if (!value) {
+                    return "std::nullopt (DataNode)";
+                }
+
+                return std::string{value->path()};
+            }
+
+            std::string operator()(const Decimal64& value)
+            {
+                return "Decimal64{"s + std::to_string(value.number) + ", " + std::to_string(value.digits) + "}";
+            }
+
+            std::string operator()(const std::vector<Bit>& value)
+            {
+                std::ostringstream oss;
+                oss << "Bits{";
+                std::transform(value.begin(), value.end(), std::experimental::make_ostream_joiner(oss, ", "), [] (const Bit& bit) {
+                    return bit.name + "(" + std::to_string(bit.position) + ")";
+                });
+                oss << "}";
+                return oss.str();
+            }
+
+            std::string operator()(const Enum& value)
+            {
+                return "Enum{"s + value.name + "}";
+            }
+
+            std::string operator()(const IdentityRef& value)
+            {
+                return "IdentityRef{"s + value.module + ":" + value.name + "}";
+            }
+
+            template <typename Type>
+            std::string operator()(const Type& value)
+            {
+                return std::to_string(value);
+            }
+        };
+    }
+    doctest::String toString(const Value& value) {
+        auto str = std::visit(impl_toStruct{}, value);
+        return str.c_str();
+    }
+}
 
 const auto example_schema = R"(
 module example-schema {
@@ -16,18 +90,185 @@ module example-schema {
     namespace "http://example.com/";
     prefix coze;
 
+    leaf dummy {
+        type string;
+    }
+
+    leaf leafInt8 {
+        description "A 8-bit integer leaf.";
+        type int8;
+    }
+
+    leaf leafInt16 {
+        description "A 16-bit integer leaf.";
+        type int16;
+    }
+
     leaf leafInt32 {
         description "A 32-bit integer leaf.";
         type int32;
     }
 
+    leaf leafInt64 {
+        description "A 64-bit integer leaf.";
+        type int64;
+    }
+
+    leaf leafUInt8 {
+        description "A 8-bit unsigned integer leaf.";
+        type uint8;
+    }
+
+    leaf leafUInt16 {
+        description "A 16-bit unsigned integer leaf.";
+        type uint16;
+    }
+
+    leaf leafUInt32 {
+        description "A 32-bit unsigned integer leaf.";
+        type uint32;
+    }
+
+    leaf leafUInt64 {
+        description "A 64-bit unsigned integer leaf.";
+        type uint64;
+    }
+
+    leaf leafBool {
+        description "A boolean.";
+        type boolean;
+    }
+
+    leaf leafString {
+        description "A string.";
+        type string;
+    }
+
+    leaf leafEmpty {
+        description "An `empty` leaf.";
+        type empty;
+    }
+
+    leaf leafDecimal {
+        description "A decimal value.";
+        type decimal64 {
+            fraction-digits 5;
+        }
+    }
+
+    leaf leafBinary {
+        description "A binary value.";
+        type binary;
+    }
+
+    leaf intOrString {
+        description "An int or a string.";
+        type union {
+            type int32;
+            type string;
+        }
+    }
+
+    list person {
+        key 'name';
+        leaf name {
+            type string;
+        }
+    }
+
+    leaf bossPerson {
+        type leafref {
+            path '../person/name';
+        }
+    }
+
+    leaf targetInstance {
+        type instance-identifier;
+    }
+
+    leaf NOtargetInstance {
+        type instance-identifier {
+            require-instance false;
+        }
+    }
+
     leaf active {
         type boolean;
+    }
+
+    leaf flagBits {
+        type bits {
+            bit carry;
+            bit sign;
+            bit overflow;
+        }
+    }
+
+    leaf pizzaSize {
+        type enumeration {
+            enum large;
+            enum medium;
+            enum small;
+        }
+    }
+
+    identity food {
+    }
+
+    identity fruit {
+        base "food";
+    }
+
+    identity pizza {
+        base "food";
+    }
+
+    identity hawaii {
+        base "pizza";
+    }
+
+    typedef foodTypedef {
+        type identityref {
+            base food;
+        }
+    }
+
+    leaf leafFoodTypedef {
+        type foodTypedef;
     }
 })";
 
 const auto data = R"({
   "example-schema:leafInt32": 420
+}
+)";
+
+const auto dataTypes = R"({
+  "example-schema:leafInt8": -43,
+  "example-schema:leafInt16": 3000,
+  "example-schema:leafInt32": -391203910,
+  "example-schema:leafInt64": "-234214214928",
+  "example-schema:leafUInt8": 43,
+  "example-schema:leafUInt16": 2333,
+  "example-schema:leafUInt32": 23423422,
+  "example-schema:leafUInt64": "453545335344",
+  "example-schema:leafBool": false,
+  "example-schema:leafString": "AHOJ",
+  "example-schema:leafEmpty": [null],
+  "example-schema:leafDecimal": "23212131231.43242",
+  "example-schema:leafBinary": "AAAABBBBCCC=",
+  "example-schema:intOrString": 14332,
+  "example-schema:person": [
+    {
+        "name": "Dan"
+    }
+  ],
+  "example-schema:bossPerson": "Dan",
+  "example-schema:targetInstance": "/example-schema:leafBool",
+  "example-schema:NOtargetInstance": "/example-schema:dummy",
+  "example-schema:flagBits": "carry overflow",
+  "example-schema:pizzaSize": "large",
+  "example-schema:leafFoodTypedef": "hawaii"
 }
 )";
 
@@ -80,10 +321,120 @@ TEST_CASE("Data Node manipulation")
 
     DOCTEST_SUBCASE("DataNodeTerm")
     {
-        auto node = ctx.parseDataMem(data, libyang::DataFormat::JSON);
-        auto term = node.findPath("/example-schema:leafInt32")->asTerm();
-        REQUIRE(term.path() == "/example-schema:leafInt32");
-        REQUIRE(term.valueStr() == "420");
+        auto data = ctx.parseDataMem(dataTypes, libyang::DataFormat::JSON);
+        std::string path;
+        libyang::Value expected;
+
+        DOCTEST_SUBCASE("value types") {
+            DOCTEST_SUBCASE("int8") {
+                path = "/example-schema:leafInt8";
+                expected = int8_t{-43};
+            }
+
+            DOCTEST_SUBCASE("int16") {
+                path = "/example-schema:leafInt16";
+                expected = int16_t{3000};
+            }
+
+            DOCTEST_SUBCASE("int32") {
+                path = "/example-schema:leafInt32";
+                expected = int32_t{-391203910};
+            }
+
+            DOCTEST_SUBCASE("int64") {
+                path = "/example-schema:leafInt64";
+                expected = int64_t{-234214214928};
+            }
+
+            DOCTEST_SUBCASE("uint8") {
+                path = "/example-schema:leafUInt8";
+                expected = uint8_t{43};
+            }
+
+            DOCTEST_SUBCASE("uint16") {
+                path = "/example-schema:leafUInt16";
+                expected = uint16_t{2333};
+            }
+
+            DOCTEST_SUBCASE("uint32") {
+                path = "/example-schema:leafUInt32";
+                expected = uint32_t{23423422};
+            }
+
+            DOCTEST_SUBCASE("uint64") {
+                path = "/example-schema:leafUInt64";
+                expected = uint64_t{453545335344};
+            }
+
+            DOCTEST_SUBCASE("decimal64") {
+                path = "/example-schema:leafDecimal";
+                using namespace libyang::literals;
+                expected = 23212131231.43242_decimal64;
+            }
+
+            DOCTEST_SUBCASE("boolean") {
+                path = "/example-schema:leafBool";
+                expected = bool{false};
+            }
+
+            DOCTEST_SUBCASE("string") {
+                path = "/example-schema:leafString";
+                expected = std::string{"AHOJ"};
+            }
+
+            DOCTEST_SUBCASE("empty") {
+                path = "/example-schema:leafEmpty";
+                expected = libyang::Empty{};
+            }
+
+            DOCTEST_SUBCASE("binary") {
+                path = "/example-schema:leafBinary";
+                expected = libyang::Binary{{0, 0, 0, 4, 16, 65, 8, 32}, "AAAABBBBCCC="};
+            }
+
+            DOCTEST_SUBCASE("intOrString") {
+                path = "/example-schema:intOrString";
+                expected = int32_t{14332};
+            }
+
+            DOCTEST_SUBCASE("leafref") {
+                path = "/example-schema:bossPerson";
+                expected = std::string{"Dan"};
+            }
+
+            DOCTEST_SUBCASE("instance-identifier") {
+                DOCTEST_SUBCASE("require-instance = true") {
+                    path = "/example-schema:targetInstance";
+                    expected = data.findPath("/example-schema:leafBool");
+                }
+
+                DOCTEST_SUBCASE("require-instance = false") {
+                    path = "/example-schema:NOtargetInstance";
+                    expected = std::nullopt;
+                }
+            }
+
+            DOCTEST_SUBCASE("bits") {
+                path = "/example-schema:flagBits";
+                expected = std::vector<libyang::Bit>{{0, "carry"}, {2, "overflow"}};
+            }
+
+            DOCTEST_SUBCASE("enum") {
+                path = "/example-schema:pizzaSize";
+                expected = libyang::Enum{"large"};
+            }
+
+            DOCTEST_SUBCASE("identityref") {
+                path = "/example-schema:leafFoodTypedef";
+                expected = libyang::IdentityRef{"example-schema", "hawaii"};
+            }
+        }
+
+        auto node = data.findPath(path.c_str());
+        REQUIRE(node);
+        auto term = node->asTerm();
+        REQUIRE(term.path() == path);
+        REQUIRE(term.value() == expected);
     }
 
     DOCTEST_SUBCASE("newPath")

include/libyang-cpp/DataNode.hpp
@@ -11,6 +11,7 @@
 #include <optional>
 #include <libyang-cpp/Enum.hpp>
 #include <libyang-cpp/String.hpp>
+#include <libyang-cpp/Value.hpp>
 
 struct lyd_node;
 struct ly_ctx;
@@ -39,6 +40,9 @@ public:
     std::optional<DataNode> newPath(const char* path, const char* value = nullptr, const std::optional<CreationOptions> options = std::nullopt);
 
     friend Context;
+    friend DataNodeTerm;
+
+    bool operator==(const DataNode& node) const;
 
     friend std::optional<DataNode> impl::newPath(lyd_node* node, ly_ctx* parent, std::shared_ptr<internal_empty> viewCount, const char* path, const char* value, const std::optional<CreationOptions> options);
 
@@ -62,6 +66,8 @@ public:
     std::string_view valueStr() const;
 
     friend DataNode;
+    Value value() const;
+
 private:
     using DataNode::DataNode;
 };

include/libyang-cpp/Value.hpp
@@ -1,0 +1,217 @@
+/*
+ * Copyright (C) 2021 CESNET, https://photonics.cesnet.cz/
+ *
+ * Written by Václav Kubernát <kubernat@cesnet.cz>
+ * Written by Jan Kundrát <jan.kundrat@cesnet.cz>
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+*/
+#pragma once
+#include <cstdint>
+#include <optional>
+#include <string>
+#include <variant>
+#include <vector>
+
+namespace libyang {
+/**
+ * @brief Represents a YANG value of type `empty`.
+ */
+struct Empty {
+    auto operator<=>(const Empty&) const = default;
+};
+
+/**
+ * Represents a YANG value of type `binary` as raw bytes and as a base64 string.
+ */
+struct Binary {
+    auto operator<=>(const Binary&) const = default;
+    std::vector<uint8_t> data;
+    std::string base64;
+};
+
+struct Bit {
+    auto operator<=>(const Bit&) const = default;
+    uint32_t position;
+    std::string name;
+};
+
+struct Enum {
+    auto operator<=>(const Enum&) const = default;
+    std::string name;
+};
+
+struct IdentityRef {
+    auto operator<=>(const IdentityRef&) const = default;
+    std::string module;
+    std::string name;
+};
+
+struct Decimal64;
+namespace impl {
+constexpr int64_t abs(const int64_t number)
+{
+    return number < 0 ? -number : number;
+}
+constexpr int64_t pow10int(const uint8_t digits)
+{
+    int64_t exponent = 1;
+    for (uint8_t i = 0; i < digits; ++i) {
+        exponent *= 10;
+    }
+    return exponent;
+}
+static_assert(pow10int(0) == 1);
+static_assert(pow10int(1) == 10);
+static_assert(pow10int(2) == 100);
+
+constexpr double pow10double(const int digits)
+{
+    auto exponent = pow10int(abs(digits));
+    return digits < 0 ? 1.0 / exponent : 1.0 * exponent;
+}
+static_assert(pow10double(1) == 10);
+static_assert(pow10double(2) == 100);
+static_assert(pow10double(-1) == 0.1);
+static_assert(pow10double(-2) == 0.01);
+
+constexpr long long llround(double x)
+{
+    return (x >= 0.0) ? (long long)(x + 0.5) : (long long)(x - 0.5);
+}
+static_assert(llround(0.4999) == 0);
+static_assert(llround(0.5001) == 1);
+static_assert(llround(-0.4999) == 0);
+static_assert(llround(-0.5001) == -1);
+
+template <int64_t V, uint8_t IntegralDigits, uint8_t FractionDigitsPlusOne>
+constexpr Decimal64 make_decimal64();
+}
+
+/**
+ * Represents a YANG value of type `decimal64`.
+ */
+struct Decimal64 {
+    int64_t number;
+    uint8_t digits;
+
+    explicit constexpr operator double() const
+    {
+        return number * impl::pow10double(-digits);
+    }
+
+    constexpr Decimal64 operator-() const
+    {
+        return Decimal64{-number, digits};
+    }
+
+    template<uint8_t digits>
+    constexpr static Decimal64 fromRawDecimal(const int64_t value)
+    {
+        static_assert(digits >= 1);
+        static_assert(digits <= 18);
+        return Decimal64{value, digits};
+    }
+
+    template<uint8_t digits>
+    constexpr static Decimal64 fromDouble(const double value)
+    {
+        static_assert(digits >= 1);
+        static_assert(digits <= 18);
+        return Decimal64{impl::llround(value * impl::pow10int(digits)), digits};
+    }
+    explicit constexpr Decimal64(const int64_t number, const uint8_t digits)
+    : number(number)
+    , digits(digits)
+    {
+    }
+
+    constexpr bool operator==(const Decimal64& a) const = default;
+private:
+    template <int64_t V, uint8_t IntegralDigits, uint8_t FractionDigitsPlusOne>
+    friend constexpr Decimal64 impl::make_decimal64();
+};
+
+namespace impl {
+template <int64_t V, uint8_t IntegralDigits, uint8_t FractionDigitsPlusOne>
+constexpr Decimal64 make_decimal64()
+{
+    static_assert(IntegralDigits <= 18);
+    static_assert(IntegralDigits + FractionDigitsPlusOne <= 20);
+    if constexpr (FractionDigitsPlusOne < 2) {
+        return Decimal64::fromRawDecimal<1>(V * 10);
+    } else {
+        return Decimal64::fromRawDecimal<FractionDigitsPlusOne - 1>(V);
+    }
+}
+template <int64_t V, uint8_t IntegralDigits, uint8_t FractionDigitsPlusOne, char C, char... Cs>
+constexpr Decimal64 make_decimal64()
+{
+    static_assert((C >= '0' && C <= '9') || C == '.', "Invalid numeric character for Decimal64");
+    // more than one '.' is rejected by the lexer, apparently
+    if constexpr (C == '.') {
+        return make_decimal64<V, IntegralDigits, 1, Cs...>();
+    } else if constexpr (FractionDigitsPlusOne > 0) {
+        return make_decimal64<V * 10 + C - '0', IntegralDigits, FractionDigitsPlusOne + 1, Cs...>();
+    } else {
+        return make_decimal64<V * 10 + C - '0', IntegralDigits + 1, 0, Cs...>();
+    }
+}
+}
+inline namespace literals {
+template <char... Cs>
+constexpr Decimal64 operator "" _decimal64()
+{
+    return impl::make_decimal64<0, 0, 0, Cs...>();
+}
+
+static_assert(Decimal64::fromDouble<2>(12.34) == Decimal64::fromRawDecimal<2>(1234));
+static_assert(Decimal64::fromDouble<2>(12.34) == 12.34_decimal64);
+static_assert(double{Decimal64::fromDouble<2>(12.34)} == 12.34);
+static_assert(Decimal64::fromDouble<1>(12.34) == Decimal64::fromRawDecimal<1>(123));
+static_assert(Decimal64::fromDouble<1>(12.34) == 12.3_decimal64);
+static_assert(double{Decimal64::fromDouble<1>(12.34)} == 12.3);
+
+static_assert(123_decimal64 == Decimal64::fromRawDecimal<1>(1230));
+static_assert(12_decimal64 == Decimal64::fromRawDecimal<1>(120));
+static_assert(7_decimal64 == Decimal64::fromRawDecimal<1>(70));
+static_assert(1._decimal64 == Decimal64::fromRawDecimal<1>(10));
+static_assert(1.0_decimal64 == Decimal64::fromRawDecimal<1>(10));
+static_assert(1.00_decimal64 == Decimal64::fromRawDecimal<2>(100));
+static_assert(1.000_decimal64 == Decimal64::fromRawDecimal<3>(1000));
+static_assert(1.000000000000000000_decimal64 == Decimal64::fromRawDecimal<18>(1000000000000000000));
+static_assert(-1.000000000000000000_decimal64 == Decimal64::fromRawDecimal<18>(-1000000000000000000));
+static_assert(1.2_decimal64 == Decimal64::fromRawDecimal<1>(12));
+static_assert(12.3_decimal64 == Decimal64::fromRawDecimal<1>(123));
+static_assert(456.7_decimal64 == Decimal64::fromRawDecimal<1>(4567));
+static_assert(456.78_decimal64 == Decimal64::fromRawDecimal<2>(45678));
+static_assert(456.789_decimal64 == Decimal64::fromRawDecimal<3>(456789));
+static_assert(456.7890_decimal64 == Decimal64::fromRawDecimal<4>(4567890));
+static_assert(-456.7890_decimal64 == Decimal64::fromRawDecimal<4>(-4567890));
+}
+
+class DataNode;
+
+/**
+ * Represents a value of DataNodeTerm.
+ */
+using Value = std::variant<
+    int8_t,
+    int16_t,
+    int32_t,
+    int64_t,
+    uint8_t,
+    uint16_t,
+    uint32_t,
+    uint64_t,
+    bool,
+    Empty,
+    Binary,
+    std::string,
+    std::optional<DataNode>, // Instance identifier value.
+    Decimal64,
+    std::vector<Bit>,
+    Enum,
+    IdentityRef
+>;
+}

commit b852ea3b9a2729da364f2c4122db05d882df37f2

fix decimal64 within a union
Our code which converted `lyd_value` instances into our nice C++
variants worked directly on the leaf's own schema node's `lysc_type`,
assuming that it's a `lysc_type_dec` already. That's not the case with
unions, and one has to take a detour to arrive at the real underlying
type.

When fixing this, I also looked at the other branches of this recursive
conversion function because they are also accessing the DataNode's
members. These appear correct; they either only access the C++'s
`m_refs` (which is by definition harmless), or they get the value from
the static/dynamic storage right in the original `lyd_val`, which also
appears to be fine. Tests added, anyway.

Change-Id: I6cd938498dfc9c2eeb7d8287b779d45f87057d90
Fixes: b9113ae Implement value retrieval

src/DataNode.cpp
@@ -841,9 +841,7 @@ Value DataNodeTerm::value() const
         case LY_TYPE_UNION:
             return impl(value.subvalue->value);
         case LY_TYPE_DEC64: {
-            auto schemaDef = reinterpret_cast<const lysc_node_leaf*>(m_node->schema);
-            auto dec = reinterpret_cast<const lysc_type_dec*>(schemaDef->type);
-            return Decimal64{value.dec64, dec->fraction_digits};
+            return Decimal64{value.dec64, reinterpret_cast<const lysc_type_dec*>(value.realtype)->fraction_digits};
         }
         case LY_TYPE_BITS: {
             auto bits = valueGetSpecial<lyd_value_bits>(&value);

tests/data_node.cpp
@@ -328,6 +328,63 @@ TEST_CASE("Data Node manipulation")
                 expectedPrinter = "Dan";
             }
 
+            DOCTEST_SUBCASE("schema-specific union members")
+            {
+                using namespace libyang::literals;
+                path = "/example-schema:pwnedUnion";
+
+                DOCTEST_SUBCASE("decimal64")
+                {
+                    // the first decimal64 has fraction-digits: 1
+                    data = ctx.parseData(R"({"example-schema:pwnedUnion": "123.4"})"s, libyang::DataFormat::JSON);
+                    expected = 123.4_decimal64;
+                    expectedPrinter = "123.4";
+                }
+
+                DOCTEST_SUBCASE("nested union")
+                {
+                    // this one is nested, and it has fraction-digits: 3
+                    data = ctx.parseData(R"({"example-schema:pwnedUnion": "123.456"})"s, libyang::DataFormat::JSON);
+                    expected = 123.456_decimal64;
+                    expectedPrinter = "123.456";
+                }
+
+                DOCTEST_SUBCASE("leafref")
+                {
+                    data = ctx.parseData(R"({
+                    "example-schema:pwnedUnion": "Dan",
+                    "example-schema:person": [
+                      {
+                        "name": "Dan"
+                      }
+                    ]})"s, libyang::DataFormat::JSON);
+                    expected = "Dan"s;
+                    expectedPrinter = "Dan";
+                }
+
+                DOCTEST_SUBCASE("instance-identifier")
+                {
+                    // a nice self-referencing instance-identified that goes back to the union
+                    data = ctx.parseData(R"({"example-schema:pwnedUnion": "/example-schema:pwnedUnion"})"s, libyang::DataFormat::JSON);
+                    expected = libyang::InstanceIdentifier{"/example-schema:pwnedUnion", data->findPath("/example-schema:pwnedUnion")};
+                    expectedPrinter = "InstanceIdentifier{/example-schema:pwnedUnion}";
+                }
+
+                DOCTEST_SUBCASE("identityref")
+                {
+                    data = ctx.parseData(R"({"example-schema:pwnedUnion": "example-schema:pizza"})"s, libyang::DataFormat::JSON);
+                    expected = createIdentityRefValue(ctx, "example-schema", "pizza");
+                    expectedPrinter = "example-schema:pizza";
+                }
+
+                DOCTEST_SUBCASE("binary")
+                {
+                    data = ctx.parseData(R"({"example-schema:pwnedUnion": "AA=="})"s, libyang::DataFormat::JSON);
+                    expected = libyang::Binary{{0}, "AA=="};
+                    expectedPrinter = "AA==";
+                }
+            }
+
             DOCTEST_SUBCASE("instance-identifier")
             {
                 DOCTEST_SUBCASE("require-instance = true")

tests/example_schema.hpp
@@ -163,6 +163,32 @@ module example-schema {
         type foodTypedef;
     }
 
+    leaf pwnedUnion {
+        type union {
+            type decimal64 {
+                fraction-digits 1;
+            }
+            type leafref {
+                path '../person/name';
+            }
+            type union {
+                type instance-identifier;
+                type identityref {
+                    base food;
+                }
+                type union {
+                    type binary {
+                        length 1;
+                    }
+                    type decimal64 {
+                        fraction-digits 3;
+                    }
+                }
+            }
+        }
+    }
+
+
     container first {
         container second {
             container third {