17da45e9b219fc5a5272aeaada2edbedf1db1827
Jan Kundrát <jan.kundrat@cesnet.cz>
2026-01-16 12:31:36 +0100

    adapt to changes in libyang 4.7
    The regex handling has been changed, and the order of some built-in
    modules is now different.
    
    Change-Id: I2b790ad516254af20bb425accbeeea485b7d13d0

--- src/Regex.cpp
+++ src/Regex.cpp
@@ -6,34 +6,28 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-// The following header MUST be included before anything else which "might" use PCRE2
-// because that library uses the preprocessor to prepare the "correct" versions of symbols.
-#include <libyang/tree_data.h>
-
 #include <libyang-cpp/Regex.hpp>
-#include <pcre2.h>
+#include <libyang/tree_data.h>
 #include "utils/exception.hpp"
-
-#define THE_PCRE2_CODE_P reinterpret_cast<pcre2_code*>(this->code)
-#define THE_PCRE2_CODE_P_P reinterpret_cast<pcre2_code**>(&this->code)
 
 namespace libyang {
 
 Regex::Regex(const std::string& pattern)
     : code(nullptr)
 {
-    auto res = ly_pattern_compile(nullptr, pattern.c_str(), THE_PCRE2_CODE_P_P);
+    auto res = ly_pattern_compile(nullptr, pattern.c_str(), &code);
     throwIfError(res, ly_last_logmsg());
 }
 
 Regex::~Regex()
 {
-    pcre2_code_free(THE_PCRE2_CODE_P);
+    ly_pattern_free(code);
+    code = nullptr;
 }
 
 bool Regex::matches(const std::string& input)
 {
-    auto res = ly_pattern_match(nullptr, nullptr /* we have a precompiled pattern */, input.c_str(), input.size(), THE_PCRE2_CODE_P_P);
+    auto res = ly_pattern_match(nullptr, nullptr /* we have a precompiled pattern */, input.c_str(), input.size(), &code);
     if (res == LY_SUCCESS) {
         return true;
     } else if (res == LY_ENOT) {
